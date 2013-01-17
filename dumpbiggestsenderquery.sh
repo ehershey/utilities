6b61f29599c306078bfb0c755b6c2d353aaa92e3
Ernie Hershey <github@ernie.org>
2013-01-17 15:53:03 -0500

    initial commit

---
+++ dumpbiggestsenderquery.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+PATH=$PATH:`dirname $0`
+dumpimapjson.js | mongoimport --db erniemail --collection messages --drop
+echo 'db.messages.aggregate( [ { $group: { _id: "$headers.from", count: { "$sum": 1 } } } , { $sort: { count: 1 } }, { $unwind: "$_id" }] ).result.forEach(function(item) { print(item.count + ": " + item._id) }) ' | mongo erniemail | tail | ( echo ; cat | sed 's/.*<\(.*\)>/\1/g'  | sed 's/$/ OR /g' | tr -d \\n ; echo )  | sed 's/ OR bye *OR *$//' 
---
+++ dumpimapjson.js
@@ -0,0 +1,66 @@
+#!/usr/bin/node
+var util = require('util');
+
+var Imap = require('imap');
+
+var since = 'January 01, 2001';
+var folder = 'UNSEEN';
+//var since = 'January 01, 2012';
+//var folder = 'ALL';
+
+var authdata = require('authdata');
+
+  var imap = new Imap({
+        user: authdata.username,
+        password: authdata.password,
+        host: 'imap.gmail.com',
+        port: 993,
+        secure: true
+      });
+
+ function show(obj) {
+    return util.inspect(obj, false, Infinity);
+  }
+
+  function die(err) {
+    process.stderr.write('Uh oh: ' + err + "\n");
+    process.exit(1);
+  }
+
+  function openInbox(cb) {
+    imap.connect(function(err) {
+      if (err) die(err);
+      imap.openBox('INBOX', true, cb);
+    });
+  }
+
+  openInbox(function(err, mailbox) {
+    if (err) die(err);
+    imap.search([ folder, ['SINCE', since] ], function(err, results) {
+      if (err) die(err);
+      imap.fetch(results,
+        //{ headers: ['from', 'to', 'subject', 'date'],
+        { headers: true,
+          cb: function(fetch) {
+            var index = 0;
+            fetch.on('message', function(msg) {
+              index++;
+              process.stderr.write('.');
+              // process.stderr.write('starting message no. ' + index + "\n");
+              msg.on('headers', function(hdrs) {
+                process.stdout.write(JSON.stringify({ msg: msg, sequenceNo: msg.seqno, headers: hdrs}) + "\n");
+                // process.stderr.write('Headers for no. ' + msg.seqno + ': ' + show(hdrs) + "\n");
+              });
+              msg.on('end', function() {
+                //process.stderr.write('ending message no. ' + index + "\n");
+              });
+            });
+          }
+        }, function(err) {
+          if (err) throw err;
+          process.stderr.write('Done fetching all messages!' + "\n");
+          imap.logout();
+        }
+      );
+    });
+  });
