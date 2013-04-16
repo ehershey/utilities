02e16fc4126f84234c9963b7e81ab1ee5de1e5a3
Ernie Hershey <ernie.hershey@10gen.com>
2013-04-16 17:25:01 -0400

    adding code to call ec2

--- test_package.py
+++ test_package.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
-import boto
+import boto.ec2
+import datetime
 import os
+import time
 
+repo_base_url = 'http:#downloads-distro.mongodb.org/repo/'
+
+# parms for ec2 instance
+#
+# debian squeeze 
+#
 ami_id = 'ami-8568efec'
-repo_base_url = 'http://downloads-distro.mongodb.org/repo/'
+region = 'us-east-1'
+key_name = 'admin1';
+security_group = 'default'
+instance_type = 'm1.large'
 
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID'] 
@@ -17,4 +28,33 @@ if not AWS_SECRET_ACCESS_KEY:
   exit(2)
 
 
+conn = boto.ec2.connect_to_region(region)
+
+res = conn.run_instances(
+  ami_id,
+  key_name = key_name,
+  instance_type = instance_type,
+  security_groups=[security_group]);
+
+timeout = 60
+
+inst = res.instances[0]
+
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+instance_name = "package-test-%s" % timestamp;
+
+inst.add_tag("Name",instance_name)
+inst.update();
+
+print "instance name: %s" % instance_name
+print "instance state: %s" % inst.state
+
+while inst.state == 'pending':
+  time.sleep(2)
+  inst.update()
+  print "instance state: %s" % inst.state
+
+if inst.state != 'running':
+  print "Unknown instance state after %d seconds elapsed: %s" % (elapsed_seconds, inst.state)
+print "connecting..."
 
