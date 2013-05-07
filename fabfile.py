d97c4d8dd95cda6acd59050715c04da8f03f99d3
Ernie Hershey <ernie.hershey@10gen.com>
2013-05-07 15:03:11 -0400

    fab commands work, minor updates to package script

---
+++ fabfile.py
@@ -0,0 +1,97 @@
+from fabric.api import env, run, sudo
+import fabric.utils
+
+# redhat
+env['hosts'] = [ 'ec2-23-22-42-53.compute-1.amazonaws.com' ]
+env['user'] = 'ec2-user'
+init_script_name = "mongod"
+init_service_name = "mongod"
+pre_repo_cmd = None
+repo_string = """[10gen] 
+name=10gen Repository 
+baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64 
+gpgcheck=0 
+enabled=1"""
+repo_file = "/etc/yum.repos.d/10gen.repo"
+post_repo_cmd = None
+packages="mongo-10gen mongo-10gen-server"
+# full commands to install/uninstall packages with string replaced by packages setting above
+#
+install_cmd = "yum install --assumeyes --quiet %s"
+uninstall_cmd = "yum --assumeyes --quiet remove %s"
+
+
+
+# debian
+#env['hosts'] = [ 'ec2-174-129-57-58.compute-1.amazonaws.com' ]
+#env['user'] = 'admin'
+#init_script_name = "mongodb"
+#init_service_name = "mongodb"
+#pre_repo_cmd = "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
+#repo_string = "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen"
+#repo_file = "/etc/apt/sources.list.d/10gen.list"
+#post_repo_cmd = "apt-get --quiet --quiet --no-download update"
+#packages="mongodb-10gen"
+## full commands to install/uninstall packages with string replaced by packages setting above
+##
+#install_cmd = "apt-get --quiet --quiet install %s"
+#uninstall_cmd = "apt-get --quiet --quiet purge %s"
+
+
+
+
+env['use_ssh_config'] = True
+env['key_filename'] = "/Users/ernie/git/ops-private/ec2/aws-build/admin1.pem"
+
+
+expected_version = "2.4.3"
+
+def hello():
+      print("Hello world!")
+      run("hostname")
+
+
+def setuprepo():
+
+    if pre_repo_cmd is not None:
+      sudo(pre_repo_cmd)
+
+    run("echo '%s' | sudo tee %s " % (repo_string, repo_file))
+
+    if post_repo_cmd is not None:
+      sudo(post_repo_cmd)
+
+
+def installpackage():
+    sudo(install_cmd % packages)
+
+def verifypackage():
+    run("ls -l /etc/init.d/%s" % init_script_name)
+
+    if not is_mongod_running():
+      start_mongod()
+
+    run("ps auwx | grep -i mongo")
+
+    output = run("echo 'db.serverBuildInfo().version' | mongo --quiet")
+
+    fabric.utils.puts("expected version number:")
+    fabric.utils.puts(expected_version)
+
+    assert (output == expected_version),"Returned version not equal to expected version"
+
+def uninstallpackage():
+    sudo(uninstall_cmd % packages)
+
+def start_mongod():
+    sudo("service %s start" % init_service_name)
+
+def stop_mongod():
+    sudo("service %s stop" % init_service_name)
+
+def is_mongod_running():
+    output = run("pidof mongod", quiet = True)
+    if output.return_code == 0:
+      return True
+    else:
+      return False
--- test_package.py
+++ test_package.py
@@ -10,9 +10,17 @@ repo_base_url = 'http:#downloads-distro.mongodb.org/repo/'
 #
 # debian squeeze 
 #
-ami_id = 'ami-8568efec'
+# ami_id = 'ami-8568efec'
+# region = 'us-east-1'
+# key_name = 'admin1'
+# security_group = 'default'
+# instance_type = 'm1.large'
+
+# rhel 6.4
+#
+ami_id = 'ami-f6f16b9f'
 region = 'us-east-1'
-key_name = 'admin1';
+key_name = 'admin1'
 security_group = 'default'
 instance_type = 'm1.large'
 
@@ -34,7 +42,7 @@ res = conn.run_instances(
   ami_id,
   key_name = key_name,
   instance_type = instance_type,
-  security_groups=[security_group]);
+  security_groups=[security_group])
 
 timeout = 60
 
@@ -42,10 +50,10 @@ inst = res.instances[0]
 
 start_time = datetime.datetime.now()
 timestamp_string = start_time.strftime("%Y%m%d%H%M%S")
-instance_name = "package-test-%s" % timestamp_string;
+instance_name = "package-test-%s" % timestamp_string
 
 inst.add_tag("Name",instance_name)
-inst.update();
+inst.update()
 
 print "instance name: %s" % instance_name
 print "instance state: %s" % inst.state
@@ -63,4 +71,5 @@ print "%d seconds elapsed" % elapsed_seconds
 if inst.state != 'running':
   print "Unknown instance state: %s" % inst.state
 print "connecting..."
+print "dns name: " % inst.dns_name
 
