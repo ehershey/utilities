88139a5fd0bab3d7b74dc862abd1768a7ab0f9ce
Ernie Hershey <ernie.hershey@10gen.com>
2013-06-09 13:37:23 -0400

    add support for parsing sub-workouts of multi-workouts

--- print_runs_csv.py
+++ print_runs_csv.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python2.7
 import json
+import os.path
 import sys
+import subprocess
 
-workouts = json.load(sys.stdin)['result']['output']['workouts']
 
-for workout in workouts:
-  if 'run' in workout['workout_type_name'].lower() or 'run' in workout['workout_description'].lower() :
-    print workout['workout_date'] + ',' + workout['distance']
+def print_workouts_from_json_stream(instream):
+  output = json.load(instream)['result']['output']
+  if output.has_key('workouts'):
+    workouts = output['workouts']
+  elif output.has_key('workout'):
+    workouts = output['workout']['children']
+  else:
+    raise "Can't find workout list in results"
 
+  for workout in workouts:
+    if 'run' in workout['workout_type_name'].lower() or 'run' in workout['workout_description'].lower() :
+      print workout['workout_date'] + ',' + workout['distance']
+    if 'multi' in workout['workout_type_name'].lower() or 'multi' in workout['workout_description'].lower() :
+      # print 'need to get more info based on ' + workout['workout_type_name'] + ',' + workout['workout_description'] + ',' + workout['workout_id']
+      process = subprocess.Popen([os.path.dirname(__file__) + "/mapmyfitness.py","http://api.mapmyfitness.com/3.1/workouts/get_workout_full?workout_id=" + workout['workout_id']], stdout = subprocess.PIPE);
+
+      print_workouts_from_json_stream(process.stdout)
+      #print process.stdout.read()
+
+print_workouts_from_json_stream(sys.stdin)
