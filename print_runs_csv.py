539497ee059767f9d30c28ace89cba20e04c9cda
Ernie Hershey <ernie.hershey@10gen.com>
2013-06-09 13:47:00 -0400

    account for different date formats

--- print_runs_csv.py
+++ print_runs_csv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 import json
 import os.path
+import re
 import sys
 import subprocess
 
@@ -18,7 +19,8 @@ def print_workouts_from_json_stream(instream):
     if 'run' in workout['workout_type_name'].lower() or 'run' in workout['workout_description'].lower() :
       # adjust date if format is weird
       #
-      print workout['workout_date'] + ',' + workout['distance']
+      workout_date = re.sub(r'(\d+)/(\d+)/(\d+)',r'\3-\1-\2',workout['workout_date'])
+      print workout_date + ',' + workout['distance']
     if 'multi' in workout['workout_type_name'].lower() or 'multi' in workout['workout_description'].lower() :
       # print 'need to get more info based on ' + workout['workout_type_name'] + ',' + workout['workout_description'] + ',' + workout['workout_id']
       process = subprocess.Popen([os.path.dirname(__file__) + "/mapmyfitness.py","http://api.mapmyfitness.com/3.1/workouts/get_workout_full?workout_id=" + workout['workout_id']], stdout = subprocess.PIPE);
