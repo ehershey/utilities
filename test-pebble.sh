df8dcca5d6ba5eb0fb6a300ac4642b7ddc1a652e
Ernie Hershey <ernie.hershey@10gen.com>
2013-06-25 17:36:22 -0400

    use one subject so messages are in one thread

--- test-pebble.sh
+++ test-pebble.sh
@@ -6,4 +6,4 @@ then
 else
   export recip=ernie.hershey@10gen.com
 fi
-while true ; do date=`date`; echo $i ; echo $date; (echo test $i ; date ) | mail -s "test $i ($date)" $recip;  i=`expr $i + 1`; sleep 5 ; done
+while true ; do date=`date`; echo $i ; echo $date; (echo test $i ; date ) | mail -s "pebble test ($date)" $recip;  i=`expr $i + 1`; sleep 5 ; done
