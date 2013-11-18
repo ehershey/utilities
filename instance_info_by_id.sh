83a179f21f52b7a2ed717f6a4ba77ca3a03a5aec
Ernie Hershey <ernie.hershey@10gen.com>
2013-11-18 16:53:22 -0500

    initial version

---
+++ instance_info_by_id.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+id="$1"
+if [ ! "$id" ] 
+then
+  echo "usage: $0 <instance id>"
+  exit 2
+fi
+
+if [ ! "`echo $id | grep ^i=`" ]
+then
+  id="i-$id"
+fi
+aws ec2 describe-instances --filters '[{"Name":"instance-id","Values":["'$id'"]}]' | `dirname $0`/format_aws_json.js
