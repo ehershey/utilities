6c0c87af5a875af767b69eacb00f7a16fabc1d3a
Ernie Hershey <ernie.hershey@10gen.com>
2013-12-04 07:37:31 -0500

    Much cleanup

--- format_aws_json.js
+++ format_aws_json.js
@@ -10,6 +10,7 @@
 //
 
 var stdin_chunks = []
+var stdout_chunks = []
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
@@ -24,6 +25,55 @@ var output_tags = [ "Name", "Username", "Hostname", "started_by", "distro" ];
 
 // json fields to include in output (case sensitive) (not tags, standard ec2 metadata)
 var output_fields = [ "StateName", "InstanceId", "InstanceType", "KeyName", "PublicDnsName" ]
+
+process.stdin.on('end', function() {
+
+  var incoming_string = stdin_chunks.join("");
+  var incoming_json = JSON.parse(incoming_string);
+
+  incoming_json.Reservations.forEach(function(reservation) { 
+      reservation.Instances.forEach(function(instance) { 
+
+        var instance_tags = {};
+        if(!instance.Tags) { instance.Tags = [] }
+
+        instance.Tags.forEach(function(tag) { 
+          output_tags.forEach(function(output_tag) { 
+            output_tag_lower = output_tag.toLowerCase();
+            if(tag.Key.toLowerCase() === output_tag) {
+              instance_tags[output_tag] = tag.Value;
+            }
+          });
+        });
+
+        // set StateName field to State.Name
+        //
+        instance.StateName = instance.State.Name;
+
+        // to track whether to print a comma
+        // 
+        var printed_field = false; 
+
+        output_fields.forEach(function(output_field) { 
+          if(!instance[output_field]) { instance[output_field] = ''; }
+          if(printed_field) {
+            stdout_chunks[stdout_chunks.length] = ","
+          }
+          stdout_chunks[stdout_chunks.length] = instance[output_field]
+          printed_field = true;
+        });
+
+        output_tags.forEach(function(output_tag) { 
+          output_tag = output_tag.toLowerCase();
+          if(!instance_tags[output_tag]) { instance_tags[output_tag] = ''; }
+          stdout_chunks[stdout_chunks.length] = ","
+          stdout_chunks[stdout_chunks.length] = instance_tags[output_tag]
+        });
+        stdout_chunks[stdout_chunks.length] = "\n"
+      });
+  });
+  process.stdout.write(stdout_chunks.join(""));
+});
 
 // to track whether to print a comma
 // 
@@ -42,49 +92,9 @@ output_tags.forEach(function(output_tag) {
 });
 process.stdout.write("\n");
 
-process.stdin.on('end', function() {
-  var incoming_string = stdin_chunks.join("");
-  var incoming_json = JSON.parse(incoming_string);
-
-  incoming_json.Reservations.forEach(function(reservation) { 
-      reservation.Instances.forEach(function(instance) { 
-
-        var instance_tags = {};
-        if(!instance.Tags) { instance.Tags = [] }
-
-        instance.Tags.forEach(function(tag) { 
-          output_tags.forEach(function(output_tag) { 
-            output_tag = output_tag.toLowerCase();
-            if(tag.Key.toLowerCase() === output_tag) {
-              instance_tags[output_tag] = tag.Value;
-            }
-          });
-        });
-
-        // set StateName field to State.Name
-        //
-        instance.StateName = instance.State.Name;
-
-        // to track whether to print a comma
-        // 
-        var printed_field = false; 
-
-        output_fields.forEach(function(output_field) { 
-          if(!instance[output_field]) { instance[output_field] = ''; }
-          if(printed_field) {
-            process.stdout.write(",");
-          }
-          process.stdout.write(instance[output_field]);
-          printed_field = true;
-        });
-
-        output_tags.forEach(function(output_tag) { 
-          output_tag = output_tag.toLowerCase();
-          if(!instance_tags[output_tag]) { instance_tags[output_tag] = ''; }
-          process.stdout.write(",");
-          process.stdout.write(instance_tags[output_tag]);
-        });
-        process.stdout.write("\n");
-      });
+var signals = ['SIGINT', 'SIGPIPE','SIGHUP','SIGTERM' ]
+for(var i = 0 ; i < signals.length ; i++) { 
+  process.on(signals[i], function() {
+    console.log('Got ' + signal);
   });
-});
+}
