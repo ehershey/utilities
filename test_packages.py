67638b6467ee4b9139c320ed14dd8797f0b96456
Ernie Hershey <ernie.hershey@10gen.com>
2013-12-18 23:55:18 -0500

    starting to plan full script

---
+++ test_packages.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python
+# spin up ubuntu machine
+# install pre-req packages
+# install gpg key
+# clone repo
+# tag repo
+# run packager.py 
+# copy repo into place
+# run packager-enterprise.py into /tmp
+# copy repo into place
+# iterate through distros running all tests
+
