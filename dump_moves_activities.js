06d93caba1e2084d2d9168f8db57d613a286796f
Ernie Hershey <ernie.hershey@10gen.com>
2014-01-15 09:42:40 -0500

    New more raw dump format

---
+++ dump_moves_activities.js
@@ -0,0 +1,40 @@
+// dump_moves_activities.js
+//
+//
+// Dump Moves activities in CSV format
+//
+// Usage: 
+// mongo moves dump_moves_activities.js
+
+var collection_name = 'activities';
+
+// use moves;
+
+print("Activity Type, Start Time, Start Longitude, Start Latitude, End Time, End Longitude, End Latitude");
+
+db.getCollection(collection_name).find().toArray().forEach(function(activity) { 
+
+  var start_time = ISODate(activity.startTime.replace(/(\d\d\d\d)(\d\d)(\d\d)/, "$1-$2-$3"));
+  var end_time = ISODate(activity.endTime.replace(/(\d\d\d\d)(\d\d)(\d\d)/, "$1-$2-$3"));
+
+  var start_trackpoint = activity.trackPoints[0];
+  if(start_trackpoint) { 
+    var end_trackpoint = activity.trackPoints[activity.trackPoints.length - 1];
+    var end_longitude = end_trackpoint.lon;
+    var end_latitude = end_trackpoint.lat;
+    var start_longitude = start_trackpoint.lon;
+    var start_latitude = start_trackpoint.lat;
+  }
+  else
+  {
+    var end_longitude = "";
+    var end_latitude = "";
+    var start_longitude = "";
+    var start_latitude = "";
+  }
+
+  print(activity.activity + ", " + start_time.toString() + ", " + start_longitude + ", " + start_latitude + ", " + end_time.toString() + ", " + end_longitude + ", " + end_latitude);
+
+});
+
+
