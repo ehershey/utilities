a8d4191fbf0ef1bf7b5b153ef7eb7425767f5c3e
Ernie Hershey <ernie.hershey@10gen.com>
2014-01-17 20:49:32 -0500

    Use a proper description

--- create_snapshots.sh
+++ create_snapshots.sh
@@ -16,6 +16,10 @@ do
       #attachment.instance-id
       for volume_id in $(aws ec2 describe-volumes --filter "Name=attachment.instance-id,Values=$instance_id"  --query "Volumes[*].VolumeId" --output text)
       do
-        aws ec2 create-snapshot --volume-id $volume_id
+        script=$(basename $0)
+        user="$USER"
+        host=$(hostname)
+        description="Created with $script by $user on host $host for ec2 instance $instance_id" 
+        aws ec2 create-snapshot --volume-id $volume_id --description "$description"
       done
 done
