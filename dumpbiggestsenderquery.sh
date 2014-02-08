ebaaf80ed6b2ff941c7064c8be228f1da97dcf7f
Ernie Hershey <ernie.hershey@10gen.com>
2014-02-08 14:17:35 -0500

    don't break on email addresses without display names

--- dumpbiggestsenderquery.sh
+++ dumpbiggestsenderquery.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 PATH=$PATH:`dirname $0`
 dumpimapjson.js | mongoimport --db erniemail --collection messages --drop
-echo 'db.messages.aggregate( [ { $group: { _id: "$headers.from", count: { "$sum": 1 } } } , { $sort: { count: 1 } }, { $unwind: "$_id" }] ).result.forEach(function(item) { print(item.count + ": " + item._id) }) ' | mongo erniemail | tail | ( echo ; cat | sed 's/.*<\(.*\)>/\1/g'  | sed 's/$/ OR /g' | tr -d \\n ; echo )  | sed 's/ OR bye *OR *$//' 
+echo 'db.messages.aggregate( [ { $group: { _id: "$headers.from", count: { "$sum": 1 } } } , { $sort: { count: 1 } }, { $unwind: "$_id" }] ).result.forEach(function(item) { print(item.count + ": " + item._id) }) ' | mongo erniemail | tail | ( echo ; cat | cut -f2 -d: | sed 's/.*<\(.*\)>/\1/g'  | sed 's/$/ OR /g' | tr -d \\n ; echo ) | sed 's/ OR bye *OR *$//' 
