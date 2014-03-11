ae7ce9651f5a04c478857bb342c96fe1f7a14d47
Ernie Hershey <ernie.hershey@10gen.com>
2014-03-11 09:54:58 -0400

    Cleanup

--- simple_monitor.js
+++ simple_monitor.js
@@ -6,6 +6,7 @@
 //
 //
 
+var async = require('async');
 var jsdom = require('jsdom');
 var fs = require('fs');
 var nodemailer = require('nodemailer');
@@ -17,9 +18,9 @@ var SUBJECT = 'Simple_monitor alert!'
 
 var nodemailer_transport = nodemailer.createTransport();
 
-// For each object in url_configs, hit "url" and run the "badcell_selector" through jquery. 
+// For each object in url_configs, hit "url" and run the "cell_selector" through jquery. 
 // if anything is matched and "negated" is not true (or if nothing matches and "negated" is true), 
-// and the detail text (return value of text_finder_from_badcell_jqobj($(badcell_selector)) !== "ignore_text",
+// and the detail text (return value of text_finder_from_cell_jqobj($(cell_selector)) !== "ignore_text",
 // send email from MAILFROM to MAILTO with details on the error
 //
 //
@@ -27,75 +28,79 @@ var nodemailer_transport = nodemailer.createTransport();
 var url_configs = [ 
   { 
     url: 'https://jira.mongodb.org/issues/?jql=assignee%20%3D%20%22ernie.hershey%4010gen.com%22%20AND%20(priority%20%3D%20%22Blocker%20-%20P1%22%20OR%20priority%20%3D%20%22Critical%20-%20P2%22)%20AND%20status%20!%3D%20%22Resolved%22%20and%20status%20!%3D%20%22Closed%22%20AND%20status%20!%3D%20%22In%20Progress%22',
-    badcell_selector: "a.issue-link",
-    text_finder_from_badcell_jqobj: function(jqobj) { return jqobj.text(); },
+    cell_selector: "a.issue-link",
+    text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
     ignore_text: '',
     negated: false
   },
  
   { 
     url: 'http://buildbot.mongodb.org:8081/builders/Windows%2032-bit/builds/-1',
-    badcell_selector: "div.success:contains('( 0 secs )')",
-    text_finder_from_badcell_jqobj: function(jqobj) { return "Windows 32-bit builder in zombie state"; },
+    cell_selector: "div.success:contains('( 0 secs )')",
+    text_finder_from_cell_jqobj: function(jqobj) { return "Windows 32-bit builder in zombie state"; },
     ignore_text: '',
     negated: false
   },
  
   { 
     url: 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart/dists/dist/10gen/binary-amd64/',
-    badcell_selector: "a:contains(deb)",
-    text_finder_from_badcell_jqobj: function(jqobj) { return "Unused"; },
+    cell_selector: "a:contains(deb)",
+    text_finder_from_cell_jqobj: function(jqobj) { return "Unused"; },
     ignore_text: '',
     negated: true
   },
  
   { 
     url: 'https://jenkins.10gen.com/robots.txt',
-    badcell_selector: "body:contains(Disallow)",
-    text_finder_from_badcell_jqobj: function(jqobj) { return "Unused"; },
+    cell_selector: "body:contains(Disallow)",
+    text_finder_from_cell_jqobj: function(jqobj) { return "Unused"; },
     ignore_text: '',
     negated: true
   },
   { 
     url: 'http://mci.10gen.com/ui/',
-    badcell_selector: "#content",
-    text_finder_from_badcell_jqobj: function(jqobj) { return "Unused"; },
+    cell_selector: "#content",
+    text_finder_from_cell_jqobj: function(jqobj) { return "Unused"; },
     ignore_text: '',
     negated: true
   },
   { 
     url: 'http://mci-motu.10gen.cc:8080/api/',
-    badcell_selector: "body:contains(api)",
-    text_finder_from_badcell_jqobj: function(jqobj) { return "Unused"; },
+    cell_selector: "body:contains(api)",
+    text_finder_from_cell_jqobj: function(jqobj) { return "Unused"; },
     ignore_text: '',
     negated: true
   },
   { 
     url: 'http://mci.10gen.com/ui/hosts/',
-    badcell_selector: "td:contains(unreachable)",
-    text_finder_from_badcell_jqobj: function(jqobj) { return jqobj.parent().children().first().text(); },
+    cell_selector: "td:contains(running)",
+    // Crazy jquery magic to get a delimited string of all the unreachable hostnames
+    // 
+    // Mostly from http://bugs.jquery.com/ticket/5858
+    //
+    text_finder_from_cell_jqobj: function(jqobj) { return jqobj.parent().children(":first-child").not(":last").append(" | ").end().text(); },
     ignore_text: '',
     negated: false
   },
 
   { 
     url: 'http://www.mongodb.org/downloads',
-    badcell_selector: ".release-version",
-    text_finder_from_badcell_jqobj: function(jqobj) { return "Unused"; },
+    cell_selector: ".release-version",
+    text_finder_from_cell_jqobj: function(jqobj) { return "Unused"; },
     ignore_text: '',
     negated: true
   },
   { 
     url: 'http://buildbot.mongodb.org/buildslaves',
-    badcell_selector: ".offline",
-    text_finder_from_badcell_jqobj: function(jqobj) { return jqobj.parent().children().first().text(); },
-    ignore_text: 'bs-win32-2',
+    cell_selector: ".offline",
+    text_finder_from_cell_jqobj: function(jqobj) { return jqobj.parent().children(":first-child").text(); },
+    ignore_text: '',
     negated: false
   },
   { 
     url: 'http://buildbot-special.10gen.com/buildslaves',
-    badcell_selector: ".offline",
-    text_finder_from_badcell_jqobj: function(jqobj) { return jqobj.parent().children().first().text(); },
+    cell_selector: ".offline",
+    text_finder_from_cell_jqobj: function(jqobj) { return jqobj.parent().children(":first-child").text(); },
     ignore_text: '',
     negated: false
   },
@@ -105,22 +110,25 @@ var url_configs = [
     // Date logic to get the full month name and day number of the day that was 23 hours ago,
     // so until 11pm this will return yesterday. This can be shaky logic since most days it shouldn't
     // trigger anyways
-    badcell_selector: 'div.contrib-streak-current:contains(' + (new Date((new Date()) - 23 * 60 * 60 * 1000)).toFormat("MMMM DD") + '), ' +
+    cell_selector: 'div.contrib-streak-current:contains(' + (new Date((new Date()) - 23 * 60 * 60 * 1000)).toFormat("MMMM DD") + '), ' +
                       'div.contrib-streak-current:contains(' + (new Date()).toFormat("MMMM DD") + ')',
-    text_finder_from_badcell_jqobj: function(jqobj) { return "Date not found in page! (" + (new Date((new Date()) - 23 * 60 * 60 * 1000)).toFormat("MMMM DD") + ')'; },
+    text_finder_from_cell_jqobj: function(jqobj) { return "Date not found in page! (" + (new Date((new Date()) - 23 * 60 * 60 * 1000)).toFormat("MMMM DD") + ')'; },
     ignore_text: '',
     negated: true
   }
 ];
 
+var errors_found = 0;
 
-url_configs.forEach( function(url_config) { 
+async.each(url_configs, check_url, done_checking_all);
+
+function check_url(url_config, done_checking_one) {
   var url = url_config.url;
-  var badcell_selector = url_config.badcell_selector;
+  var cell_selector = url_config.cell_selector;
   var ignore_text = url_config.ignore_text;
   var negated = url_config.negated;
 
-  var text_finder_from_badcell_jqobj = url_config.text_finder_from_badcell_jqobj;
+  var text_finder_from_cell_jqobj = url_config.text_finder_from_cell_jqobj;
 
   console.log('Starting request for url: ' + url);
 
@@ -142,15 +150,15 @@ url_configs.forEach( function(url_config) {
     }
     else {
 
-      console.log("looking for bad cells with selector: " + badcell_selector);
-      var badcells = window.$(badcell_selector);
+      console.log("looking for cells with selector: " + cell_selector);
+      var cells = window.$(cell_selector);
       var detail_text;
 
-      console.log('found badcells: ' + badcells.length);
+      console.log('found cells: ' + cells.length);
 
-      if( ( badcells.length && !negated ) || ( !badcells.length && negated ) ) { 
+      if( ( cells.length && !negated ) || ( !cells.length && negated ) ) { 
 
-        detail_text = text_finder_from_badcell_jqobj(badcells);
+        detail_text = text_finder_from_cell_jqobj(cells);
 
         if(ignore_text && detail_text === ignore_text) {
           console.log('detail_text === ignore_text; ignoring error (' + detail_text + ')');
@@ -161,13 +169,21 @@ url_configs.forEach( function(url_config) {
               from: MAILFROM,
               to: MAILTO,
               subject: SUBJECT,
-              text: 'Error checking URL: ' + url + "\nBad cell count: " + badcells.length + "\nError detail text: " + detail_text,
-              html: '<b>Error checking URL: <a href="' + url + '">' + url + '</a></b><br/>Bad cell length: ' + badcells.length + '<br/>Error detail text: ' + detail_text
+              text: 'Error checking URL: ' + url + "\nMatching cell count: " + cells.length + "\nError detail text: " + detail_text,
+              html: '<b>Error checking URL: <a href="' + url + '">' + url + '</a></b><br/>Matching cell length: ' + cells.length + '<br/>Error detail text: ' + detail_text
           });
+          errors_found++;
         }
       }
     }
+    done_checking_one();
   });
-})
+}
+
+function done_checking_all(err) {
+  if(err) throw err;
+  console.log('Total errors found: ' + errors_found);
+}
+
 
 
