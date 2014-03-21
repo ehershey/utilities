d37dae5b2b7f9d0300a82f1aa5533960764ebb3f
Ernie Hershey <ernie.hershey@10gen.com>
2014-03-21 16:32:06 -0400

    remove old filenames

--- verify_mongodb_push_versions.sh
+++ verify_mongodb_push_versions.sh
@@ -26,19 +26,19 @@ s3://downloads.10gen.com/linux/mongodb-linux-x86_64-enterprise-suse11-%s.tgz
 s3://downloads.10gen.com/linux/mongodb-linux-x86_64-enterprise-ubuntu1204-%s.tgz
 s3://downloads.10gen.com/win32/mongodb-win32-x86_64-enterprise-windows-64-%s.msi
 s3://downloads.mongodb.org/linux/mongodb-linux-i686-%s.tgz
-s3://downloads.mongodb.org/linux/mongodb-linux-i686-debugsymbols-%s.tgz
-s3://downloads.mongodb.org/linux/mongodb-linux-i686-rhel57-%s.tgz
-s3://downloads.mongodb.org/linux/mongodb-linux-i686-static-%s.tgz
 s3://downloads.mongodb.org/linux/mongodb-linux-x86_64-%s.tgz
-s3://downloads.mongodb.org/linux/mongodb-linux-x86_64-debugsymbols-%s.tgz
-s3://downloads.mongodb.org/linux/mongodb-linux-x86_64-legacy-%s.tgz
-s3://downloads.mongodb.org/linux/mongodb-linux-x86_64-rhel57-%s.tgz
 s3://downloads.mongodb.org/win32/mongodb-win32-i386-%s.zip
 s3://downloads.mongodb.org/win32/mongodb-win32-x86_64-%s.zip
 s3://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-%s.zip
 s3://downloads.mongodb.org/osx/mongodb-osx-x86_64-%s.tgz
 s3://downloads.mongodb.org/sunos5/mongodb-sunos5-x86_64-%s.tgz
 "
+# s3://downloads.mongodb.org/linux/mongodb-linux-x86_64-legacy-%s.tgz
+# s3://downloads.mongodb.org/linux/mongodb-linux-x86_64-rhel57-%s.tgz
+# s3://downloads.mongodb.org/linux/mongodb-linux-i686-rhel57-%s.tgz
+# s3://downloads.mongodb.org/linux/mongodb-linux-i686-static-%s.tgz
+# s3://downloads.mongodb.org/linux/mongodb-linux-i686-debugsymbols-%s.tgz
+# s3://downloads.mongodb.org/linux/mongodb-linux-x86_64-debugsymbols-%s.tgz
 
 tried=0
 found=0
