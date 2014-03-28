834c16764e5f36783edff39797b1dcb5cf7612f0
Ernie Hershey <ernie.hershey@10gen.com>
2014-03-28 10:59:15 -0400

    imported from dropbox

---
+++ file_mp3s.sh
@@ -0,0 +1,135 @@
+#!/bin/sh
+# usage: file_mp3s.sh <source> <target>
+#
+#
+# for example: 
+#
+# file_mp3s.sh [ --nocache ] "/cygdrive/x/LCD Soundsystem" /cygdrive/x
+#
+#
+# Depends on:
+# generate_normalized_mp3_path.sh in path
+
+nocache=""
+dryrun=""
+
+while [ "$3" ]
+do
+  if [ "$1" = "--nocache" ]
+  then
+    nocache=1
+    shift
+  elif [ "$1" = "--dryrun" ]
+  then
+    dryrun=1
+    shift
+  else
+    echo "Invalid argument: $1"
+    exit 2
+  fi
+done
+
+source="$1"
+target="$2"
+
+if [ ! "$source" -o ! "$target" ]
+then
+  echo "usage: $0 <source> <target>"
+  exit 2
+fi
+
+tempfile=/tmp/file_mp3s_list.$$..txt
+DEFAULT_CACHE_TIMEOUT=300
+
+if [ "$nocache" ]
+then
+  cache_timeout=0
+else
+  cache_timeout=$DEFAULT_CACHE_TIMEOUT
+fi
+
+cache_run $cache_timeout "find \"$source\" -type f" > $tempfile
+
+
+total_files=`wc -l < $tempfile`
+echo "total_files: $total_files"
+
+i=0
+while read file
+do 
+  echo "examining $file" 
+  i=`expr $i + 1`
+  echo "($i of $total_files)"
+  newfile="`cache_run 86400 \"generate_normalized_mp3_path.sh  \\\"$file\\\"\"`"
+  if [ ! "$newfile" ]
+  then
+    echo "no path generated"
+    continue
+  fi
+  newfile="$target/$newfile"
+  lcfile="`echo \"$file\" | tr A-Z a-z | sed 's#//#/#g'`"
+  lcnewfile="`echo \"$newfile\" | tr A-Z a-z | sed 's#//#/#g'`"
+  echo "file: >$file<"
+  echo "newfile: >$newfile<"
+  if [ "$lcfile" = "$lcnewfile" ] 
+  then 
+    echo "file is already in generated path"
+    continue
+  else  
+    ls -l "$file" "$newfile"  2>/dev/null
+    newdir="`dirname \"$newfile\"`"
+    if [ ! -e "$newdir" ]
+    then
+      mkdir -p "$newdir"
+    fi
+    if [ ! -e "$newfile" ]
+    then
+      echo moving file
+      echo mv "$file" "$newfile" 
+      if [ ! "$dryrun" ] 
+      then
+        mv "$file" "$newfile" 
+      fi
+    else
+      oldsize="`ls -l \"$file\" | awk '{print $5}'`"
+      newsize="`ls -l \"$newfile\" | awk '{print $5}'`"
+      if [ "$oldsize" = "$newsize" -a -e "$file" -a "`wc -c < \"$file\"`" -ne 0 ]
+      then
+        echo "files are same size, deleting old version"
+        echo rm "$file"
+        if [ ! "$dryrun" ] 
+        then
+          rm "$file"
+        fi
+      else
+        # echo "sizes of old ($oldsize) and new ($newsize) differ, not sure what to do!"
+        if [ "$oldsize" -lt "$newsize" -a -e "$newfile" -a "`wc -c < \"$newfile\"`" -ne 0 ] # We know newfile exists from check above, but check again for clarity
+        then
+          echo "deleting smaller file ($file)"
+          echo rm "$file"
+          if [ ! "$dryrun" ] 
+          then
+            rm "$file"
+          fi
+        fi
+        if [ "$newsize" -lt "$oldsize"  -a -e "$file" -a "`wc -c < \"$file\"`" -ne 0 ]
+        then
+          echo "deleting smaller file ($newfile)"
+          echo rm "$newfile"
+          if [ ! "$dryrun" ] 
+          then
+              rm "$newfile"
+          fi
+          echo mv "$file" "$newfile"
+          if [ ! "$dryrun" ] 
+          then
+            mv "$file" "$newfile"
+          fi
+        fi
+      fi
+    fi
+  fi
+done < $tempfile
+
+perl -MFile::Find -e"finddepth(sub{rmdir},shift)" "$source"
+
---
+++ generate_normalized_mp3_path.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+#
+#
+# take a file, generate a standard path for it using the id3v2 utility
+#
+
+
+# --noconvert disables attempting to convert idv2 to idv3
+#
+if [ "$1" = "--noconvert" ]
+then
+  noconvert=1
+  shift
+else
+  noconvert=""
+fi
+
+
+file="$1"
+
+
+if [ ! "$file" ]
+then
+  echo "usage: $0 <filename>"
+  exit 3
+fi
+
+extension=`echo "$file" | sed 's/.*\.//'`
+
+if which id3info >/dev/null 2>&1
+then
+  artist=`id3info "$file" | grep  "Band/orchestra/accompaniment" | cut -f2 -d: | sed 's/^[ 	]*\(.*\)[ 	]*$/\1/g'`
+
+  if [ ! "$artist" ]
+  then
+    artist=`id3info "$file" | grep  "=== TPE2" | cut -f2 -d: | sed 's/^[ 	]*\(.*\)[ 	]*$/\1/g'`
+  fi
+  if [ ! "$artist" ]
+  then
+    artist=`id3info "$file" | grep  "=== TP1" | cut -f2 -d: | sed 's/^[ 	]*\(.*\)[ 	]*$/\1/g'`
+  fi
+  if [ ! "$artist" ]
+  then
+    artist=`id3info "$file" | grep  "=== TPE1" | cut -f2 -d: | sed 's/^[ 	]*\(.*\)[ 	]*$/\1/g'`
+  fi
+  title=`id3info "$file" | grep  "Title/songname/content description" | cut -f2 -d: | sed 's/^[ 	]*\(.*\)[ 	]*$/\1/g'`
+  album=`id3info "$file" | grep  "Album/Movie/Show title" | cut -f2 -d: | sed 's/^[ 	]*\(.*\)[ 	]*$/\1/g'`
+  track=`id3info "$file" | grep  "Track number/Position in set" | cut -f2 -d: | sed 's/^[ 	]*\(.*\)[ 	]*$/\1/g' | cut -f1 -d/ | sed 's/^0*//'`
+else
+
+  if [ "`id3v2  -R "$file" 2>/dev/null | grep 'No ID3 tag'`" ]
+  then
+    title=`id3 -l -R "$file" 2>/dev/null | grep ^Title: | cut -f2- -d\  | sed 's/[ 	]*$//'`
+    artist=`id3 -l -R "$file" 2>/dev/null | grep ^Artist: | cut -f2- -d\  | sed 's/[ 	]*$//'`
+    album=`id3 -l -R "$file" 2>/dev/null | grep ^Album: | cut -f2- -d\  | sed 's/[ 	]*$//'`
+    track=`id3 -l -R "$file" 2>/dev/null | grep ^Track: | cut -f2- -d\  | sed 's/[ 	]*$//' | sed 's/^0*//'`
+  else
+
+
+    artist=`id3v2 -R "$file" 2>/dev/null | egrep TP2\|TPE2 | cut -f2- -d\  | sed 's/[ 	]*$//'`
+    if [ ! "$artist" ]
+    then
+      artist=`id3v2 -R "$file" 2>/dev/null | egrep TP1\|TPE1 | cut -f2- -d\  | sed 's/[ 	]*$//'`
+    fi
+
+    album=`id3v2 -R "$file" 2>/dev/null | grep TAL | cut -f2- -d\  | sed 's/[ 	]*$//'`
+    title=`id3v2 -R "$file" 2>/dev/null | egrep ^TT2:\|^TIT2: | cut -f2- -d\  | sed 's/[ 	]*$//'`
+    track=`id3v2 -R "$file" 2>/dev/null | egrep ^TRK:\|^TRCK: | cut -f2- -d\  | cut -f1 -d/ | sed 's/[ 	]*$//' | sed 's/^0*//'`
+  fi
+
+  if [ ! "$artist" -a ! "$album" -a ! "$title" -a ! "$track" ]
+  then
+    artist=`id3v2 -l "$file" | egrep TP2\|TPE2 | sed 's/.*: //'`
+    if [ ! "$artist" ]
+    then
+      artist=`id3v2 -l "$file" | egrep TP1\|TPE1 | sed 's/.*: //'`
+    fi
+    album=`id3v2 -l "$file"| grep TAL | cut -f2- -d: | sed 's/^ //'`
+    title=`id3v2 -l "$file"  | egrep ^TT2\|^TIT2 | sed 's/.*: //'`
+    track=`id3v2 -l "$file" | egrep ^TRK\|^TRCK | sed 's/.*: //' | sed 's/\/.*//' | sed 's/^0*//'`
+  fi
+fi
+
+  #echo "artist: $artist" >&2
+  #echo "album: $album" >&2
+  #echo "title: $title" >&2
+  #echo "track: $track" >&2
+
+dirname="$artist/$album"
+if [ "$track" ]
+then
+  filename=`printf "%02d " $track` 
+fi
+filename="$filename$title.$extension"
+
+if [ "$title" -a "$artist" -a "$album" ]
+then
+  echo "$dirname/$filename"
+else 
+  echo "Can't determine file info ($artist/$album/$title)" >&2
+  if [ "$(which id3v2 2>/dev/null)" -a "$extension" = "mp3" -a ! "$noconvert" ]
+  then
+    id3v2 -C "$file" >&2
+    $0 --noconvert "$file"
+    exit $?
+  else
+    exit 4
+  fi
+fi
+
+
+
