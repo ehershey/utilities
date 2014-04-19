6c2c9d807d1c0d0dbb6f8a66654d54c5daa01c87
Ernie Hershey <github@ernie.org>
2014-04-18 23:37:13 -0400

    new crontab for secondary server

---
+++ load_crontab_secondary.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# load crontab from crontab-secondary.txt and crontab.env
+#
+#
+CRON_USER=ernie
+FILEHOME=/home/ernie/git/utilities
+
+if [ "$CRON_USER" != "$USER" ] 
+then
+  command="sudo crontab -u $CRON_USER"
+else
+  command="crontab"
+fi
+
+if [ ! -e "$FILEHOME/crontab.env" -o ! -e "$FILEHOME/crontab-secondary.txt" ] 
+then
+  echo "Can't find crontab.env or crontab-secondary.txt in $FILEHOME"
+  exit 2
+fi
+cat "$FILEHOME/crontab.env" "$FILEHOME/crontab-secondary.txt" | $command 
