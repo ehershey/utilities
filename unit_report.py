2653f9ecf6732f60f6d03b1d28a24110b7b6f4db
Ernie Hershey <github@ernie.org>
2014-05-09 10:40:21 -0400

    add yesterday

--- unit_report.py
+++ unit_report.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python
 import datetime
 import os
+import os.path
+import time
+from os.path import expanduser
+home = expanduser("~")
+
+
+TEMPLATE_FILENAME = "%s/unit-report-template.html" % os.path.dirname(os.path.realpath(__file__)) 
+MOVES_CSV_FILENAME = "%s/Dropbox/Web/moves.csv" % home
 
 placeholder = {}
 
-placeholder['units_average'] = os.popen("cut -f5 -d, ~/Dropbox/Web/moves.csv  | awk '{ total += $1; count++ } END { print total/count }' | sed 's/\..*//'").read()
-placeholder['units_today'] = os.popen("cut -f5 -d, ~/Dropbox/Web/moves.csv  | head -2 | tail -1 | sed 's/\..*//'").read()
-placeholder['units_average_2013'] = os.popen("grep ^2013- ~/Dropbox/Web/moves.csv | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }' | sed 's/\..*//'").read()
-placeholder['units_average_2014'] = os.popen("grep ^2014- ~/Dropbox/Web/moves.csv | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }' | sed 's/\..*//'").read()
-placeholder['units_average_7days'] = os.popen("head -8 ~/Dropbox/Web/moves.csv | tail -7 | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }' | sed 's/\..*//'").read()
-placeholder['now'] = str(datetime.datetime.now())
+units_average = os.popen("cut -f5 -d, %s| awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read()
+units_today = os.popen("cut -f5 -d, %s  | head -2 | tail -1" % MOVES_CSV_FILENAME).read()
+units_yesterday = os.popen("cut -f5 -d, %s  | head -3 | tail -1" % MOVES_CSV_FILENAME).read()
+units_average_2013 = os.popen("grep ^2013- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read()
+units_average_2014 = os.popen("grep ^2014- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read()
+units_average_7days = os.popen("head -8 %s | tail -7 | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read()
+
+
+units_today_2013_diff = float(units_today) - float(units_average_2013)
+units_yesterday_2013_diff = float(units_yesterday) - float(units_average_2013)
+
+placeholder['units_today_2013_diff'] = units_today_2013_diff 
+placeholder['units_today'] = units_today 
+placeholder['units_yesterday_2013_diff'] = units_yesterday_2013_diff 
+placeholder['units_yesterday'] = units_yesterday 
+placeholder['units_average'] = units_average 
+placeholder['units_average_2013'] = units_average_2013 
+placeholder['units_average_2014'] = units_average_2014 
+placeholder['units_average_7days'] = units_average_7days 
+placeholder['now'] = time.ctime()
+placeholder['moves_csv_modified'] = time.ctime(os.path.getmtime(MOVES_CSV_FILENAME))
 
 
 # echo "units_today: $units_today"
@@ -18,7 +41,6 @@ placeholder['now'] = str(datetime.datetime.now())
 # echo "units_average_2014: $units_average_2014"
 # echo "units_average_7days: $units_average_7days"
 
-TEMPLATE_FILENAME = "%s/unit-report-template.html" % os.path.dirname(os.path.realpath(__file__)) 
 
 if __name__ == '__main__':
     is_release_candidate = False
