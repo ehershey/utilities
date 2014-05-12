2287015bcf21bef13f0ab97649ed1ce53ae0dc44
Ernie Hershey <ernie.hershey@10gen.com>
2014-05-11 22:49:55 -0400

    call numerous, add biking

--- unit_report.py
+++ unit_report.py
@@ -10,18 +10,21 @@ import numerous_apikey
 home = expanduser("~ernie")
 
 
-TEMPLATE_FILENAME = "%s/unit-report-template.html" % os.path.dirname(os.path.realpath(__file__)) 
+TEMPLATE_FILENAME = "%s/unit-report-template.html" % os.path.dirname(os.path.realpath(__file__))
 MOVES_CSV_FILENAME = "%s/Dropbox/Web/moves.csv" % home
 
 placeholder = {}
 
-units_average = os.popen("cut -f5 -d, %s| awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
 units_today = os.popen("cut -f5 -d, %s  | head -2 | tail -1" % MOVES_CSV_FILENAME).read().rstrip()
+biked_today = os.popen("cut -f2 -d, %s  | head -2 | tail -1 | tr -d a-z" % MOVES_CSV_FILENAME).read().rstrip()
+units_average = os.popen("cut -f5 -d, %s| awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
 units_yesterday = os.popen("cut -f5 -d, %s  | head -3 | tail -1" % MOVES_CSV_FILENAME).read().rstrip()
 units_average_2013 = os.popen("grep ^2013- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
 units_average_2014 = os.popen("grep ^2014- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
 units_average_7days = os.popen("head -8 %s | tail -7 | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
 
+if biked_today == "":
+  biked_today = 0
 
 units_today_2013_diff = float(units_today) - float(units_average_2013)
 units_yesterday_2013_diff = float(units_yesterday) - float(units_average_2013)
@@ -55,17 +58,17 @@ else:
     placeholder['alltime_class'] = "negative_diff"
 
 
-placeholder['units_today_2013_diff'] = units_today_2013_diff 
-placeholder['units_today'] = units_today 
-placeholder['units_yesterday_2013_diff'] = units_yesterday_2013_diff 
-placeholder['units_yesterday'] = units_yesterday 
-placeholder['units_average'] = units_average 
-placeholder['units_average_2013_diff'] = units_average_2013_diff 
-placeholder['units_average_2013'] = units_average_2013 
-placeholder['units_average_2014'] = units_average_2014 
-placeholder['units_average_2014_2013_diff'] = units_average_2014_2013_diff 
-placeholder['units_average_7days'] = units_average_7days 
-placeholder['units_average_7days_2013_diff'] = units_average_7days_2013_diff 
+placeholder['units_today_2013_diff'] = units_today_2013_diff
+placeholder['units_today'] = units_today
+placeholder['units_yesterday_2013_diff'] = units_yesterday_2013_diff
+placeholder['units_yesterday'] = units_yesterday
+placeholder['units_average'] = units_average
+placeholder['units_average_2013_diff'] = units_average_2013_diff
+placeholder['units_average_2013'] = units_average_2013
+placeholder['units_average_2014'] = units_average_2014
+placeholder['units_average_2014_2013_diff'] = units_average_2014_2013_diff
+placeholder['units_average_7days'] = units_average_7days
+placeholder['units_average_7days_2013_diff'] = units_average_7days_2013_diff
 placeholder['now'] = time.ctime()
 placeholder['moves_csv_modified'] = time.ctime(os.path.getmtime(MOVES_CSV_FILENAME))
 
@@ -76,12 +79,33 @@ placeholder['moves_csv_modified'] = time.ctime(os.path.getmtime(MOVES_CSV_FILENA
 # echo "units_average_2014: $units_average_2014"
 # echo "units_average_7days: $units_average_7days"
 
+def post_numerous_metric(metric_id, value):
+    auth_handler = urllib2.HTTPBasicAuthHandler()
+
+    url = "https://api.numerousapp.com/v1/metrics/%s/events" % metric_id
+
+
+    auth_handler.add_password(realm = 'Numerous', uri = url, user = numerous_apikey.apikey, passwd='')
+    opener = urllib2.build_opener(auth_handler)
+    # ...and install it globally so it can be used with urlopen.
+    urllib2.install_opener(opener)
+
+    values = """ { "value": %s, "onlyIfChanged": true } """ % value
+    headers = { 'Content-Type': 'application/json' }
+    request = urllib2.Request(url, data=values, headers=headers)
+
+    try:
+      response_body = urllib2.urlopen(request).read()
+      sys.stderr.write(response_body)
+    except urllib2.URLError as e:
+      sys.stderr.write("Error updating value to %s via url: %s: %s\n" % (value, url, e))
+
+
 
 if __name__ == '__main__':
 
     with open (TEMPLATE_FILENAME, "r") as myfile:
           template=myfile.read()
-
 
     formated = template.format(**placeholder)
     print formated
@@ -90,19 +114,5 @@ if __name__ == '__main__':
     #
     # TODO: move this out of report script
     #
-    metric_id = "6359390767342201980"
-    url = "https://api.numerousapp.com/v1/metrics/%s/events" % metric_id
-    auth_handler = urllib2.HTTPBasicAuthHandler()
-    auth_handler.add_password(realm = 'Numerous',
-                                  uri = url, 
-                                  user = numerous_apikey.apikey,
-                                  passwd='')
-    opener = urllib2.build_opener(auth_handler)
-    # ...and install it globally so it can be used with urlopen.
-    urllib2.install_opener(opener)
-
-    values = """ { "value": %s, "onlyIfChanged": true } """ % units_today
-    headers = { 'Content-Type': 'application/json' }
-    request = urllib2.Request(url, data=values, headers=headers)
-    response_body = urllib2.urlopen(request).read()
-    sys.stderr.write(response_body)
+    post_numerous_metric(6359390767342201980, units_today)
+    post_numerous_metric(7670190745339240677, biked_today)
