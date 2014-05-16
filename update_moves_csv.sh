dca87c804c5286d8fb928af00abf1578f2f7c957
Ernie Hershey <ernie.hershey@10gen.com>
2014-05-16 06:39:41 -0400

    add /usr/sbin to path

--- update_moves_csv.sh
+++ update_moves_csv.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Update moves.csv with values from Moves Service
 # 
 # Requires $MOVES_ACCESS_TOKEN environment variable
@@ -14,5 +14,6 @@ then
   exit 2
 fi
 curl "https://api.moves-app.com/api/v1/user/summary/daily?pastDays=10&access_token=$MOVES_ACCESS_TOKEN" | ~ernie/git/utilities/print_moves_csv.py > /tmp/moves-new.csv ; touch ~ernie/Dropbox/Web/moves.csv ; cat ~ernie/Dropbox/Web/moves.csv >> /tmp/moves-new.csv ; cat /tmp/moves-new.csv | sort --key 5,5 --field-separator=, --reverse --numeric | sort --key=1,1 --field-separator=, --reverse --unique > ~ernie/Dropbox/Web/moves.csv
+export PATH=$PATH:/usr/sbin
 chown :apache ~ernie/Dropbox/Web/moves.csv
 chmod g+rwx ~ernie/Dropbox/Web/moves.csv
