7dfb0c9cfad9a3cd2469f33b1df82af62a1a410b
Ernie Hershey <github@ernie.org>
2014-05-25 23:52:20 -0400

    post weight diff in addition to old weight

--- post_year_ago_weight_to_numerous.py
+++ post_year_ago_weight_to_numerous.py
@@ -13,6 +13,7 @@
 MARGIN_PREVIOUS_WEIGHT = 14
 
 METRIC_ID = 2541720541413967188
+DIFF_METRIC_ID = 414939844104127128
 
 import datetime
 from ernie import post_numerous_metric, get_withings_weight
@@ -23,11 +24,14 @@ def main():
 
   one_year_ago = today - datetime.timedelta(days=365)
 
-  one_year_ago_plus_margin = one_year_ago - datetime.timedelta(days = MARGIN_PREVIOUS_WEIGHT)
+  one_year_ago_minus_margin = one_year_ago - datetime.timedelta(days = MARGIN_PREVIOUS_WEIGHT)
+  today_minus_margin = today - datetime.timedelta(days = MARGIN_PREVIOUS_WEIGHT)
 
-  weights = get_withings_weight(fromdate = one_year_ago_plus_margin, todate = one_year_ago)
+  weights = get_withings_weight(fromdate = one_year_ago_minus_margin, todate = one_year_ago)
+  today_weights = get_withings_weight(fromdate = today_minus_margin, todate = today)
 
   post_numerous_metric(METRIC_ID, weights[0])
+  post_numerous_metric(DIFF_METRIC_ID, weights[0] - today_weights[0])
 
 
 if __name__ == '__main__':
