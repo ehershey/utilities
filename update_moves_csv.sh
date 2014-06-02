51a3d77ad0c25d101c1697e67ffa338027154179
Ernie Hershey <ernie.hershey@10gen.com>
2014-06-01 23:17:00 -0400

    logging

--- update_moves_csv.sh
+++ update_moves_csv.sh
@@ -16,3 +16,5 @@ fi
 echo update_moves_csv.sh starting
 date
 curl "https://api.moves-app.com/api/v1/user/summary/daily?pastDays=10&access_token=$MOVES_ACCESS_TOKEN" | ~ernie/git/utilities/print_moves_csv.py > /tmp/moves-new.csv ; touch ~ernie/Dropbox/Web/moves.csv ; cat ~ernie/Dropbox/Web/moves.csv >> /tmp/moves-new.csv ; cat /tmp/moves-new.csv | sort --key 5,5 --field-separator=, --reverse --numeric | sort --key=1,1 --field-separator=, --reverse --unique > ~ernie/Dropbox/Web/moves.csv
+echo update_moves_csv.sh finished
+date
