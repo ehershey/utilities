2199cb9fb45f223e2f51b03555f8d37c613559ec
Ernie Hershey <ernie.hershey@10gen.com>
2014-06-14 23:32:47 -0400

    fix encoding problems

--- list_numerous_metrics.py
+++ list_numerous_metrics.py
@@ -3,12 +3,14 @@
 # List user metrics from Numerous 
 #
 import numerousapp
+import codecs
 
 metrics = numerousapp.get_metrics()
 
 for metric in metrics:
+    # print metric
     print "label: %s" % metric['label']
-    print "description: %s" % metric['description']
+    print "description: %s" % metric['description'].encode('ascii', 'ignore')
     print "id: %s" % metric['id']
     print "last_value: %s" % numerousapp.get_metric_value(metric['id'])
     print ""
