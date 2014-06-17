dd551249cccf73d3409454e22f9e806c050b51fc
Ernie Hershey <ernie.hershey@10gen.com>
2014-06-17 08:17:11 -0400

    Cleanups and minor enhancements

--- numerousapp.py
+++ numerousapp.py
@@ -1,4 +1,5 @@
 import json
+import re
 import sys
 import urllib2
 import numerous_apikey
@@ -28,22 +29,30 @@ def delete_metric(metric_id):
     return response_body
 
 
-def get_metrics():
+def get_metrics(labelsearch = ''):
 
     url = "https://api.numerousapp.com/v1/users/%s/metrics" % numerous_apikey.userid
 
     request = make_auth_request(url)
 
     response_body = urllib2.urlopen(request).read()
-    metrics_json = json.loads(response_body)
-    return metrics_json
+    metrics = json.loads(response_body)
+    labelsearchre = re.compile(labelsearch, flags = re.IGNORECASE)
+    return filter(lambda x: labelsearchre.search(x['label']), metrics)
 
 
-def update_metric_value(metric_id, value):
+def update_metric_value(metric_id, value, updated = None):
 
   url = "https://api.numerousapp.com/v1/metrics/%s/events" % metric_id
 
-  values = """ { "value": %s, "onlyIfChanged": true } """ % value
+  values = { "value": value }
+
+  if updated:
+      values['updated'] = updated
+  else:
+      values['onlyIfChanged'] = True
+
+  values = json.dumps(values)
   headers = { 'Content-Type': 'application/json' }
 
   request = make_auth_request(url, data=values, headers=headers)
@@ -54,21 +63,29 @@ def update_metric_value(metric_id, value):
   except urllib2.URLError as e:
     sys.stderr.write("Error updating value to %s via url: %s: %s\n" % (value, url, e))
 
-def get_metric_value(metric_id):
-
+def get_metric_values(metric_id):
+    
   url = "https://api.numerousapp.com/v1/metrics/%s/events" % metric_id
 
   request = make_auth_request(url)
-
+ 
   try:
     response_body = urllib2.urlopen(request).read()
     response = json.loads(response_body)
-    if 'events' in response and len(response['events']) > 0:
-      return response['events'][0]['value']
+    if 'events' in response:
+        return response['events']
     else:
-      return None
+      return []
   except urllib2.URLError as e:
     sys.stderr.write("Error fetching value via url %s: %s\n" % (url, e))
+
+   
+def get_metric_value(metric_id):
+    values = get_metric_values(metric_id)
+    if len(values) > 0:
+        return values[0]
+    else:
+        return None
 
 def create_metric(label, description, kind = "number", value = None, units = None, private = True, writeable = False ):
 
