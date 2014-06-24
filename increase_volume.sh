7c87bf89a3c34ebeeae48928ad0039f143799949
Ernie Hershey <ernie.hershey@10gen.com>
2014-06-24 17:39:51 -0400

    add increase_volume.sh

---
+++ increase_volume.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+#
+# Increase volume of audio files
+#
+#
+
+if [ ! "$1" ]
+then
+  echo "usage: $0: <file> [ <file> ... ]"
+  exit 1
+fi
+
+adjusted_files=""
+for file in "$@"
+do
+  adjusted_file="adjusted-$file"
+  ffmpeg -i "$file" -af volume=12 "$adjusted_file"
+  adjusted_files="$adjusted_files\n$adjusted_file"
+done
+
+# TODO fix '-e' interpreted characters in filenames
+#
+echo -e "Adjusted files: $adjusted_files"
