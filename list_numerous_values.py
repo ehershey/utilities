dce62575df368e65c353b2c3ba9179a95575e7d0
Ernie Hershey <ernie.hershey@10gen.com>
2014-06-25 17:36:00 -0400

    allow metric label argument to value script

--- list_numerous_values.py
+++ list_numerous_values.py
@@ -7,13 +7,21 @@ import argparse
 import datetime
 
 parser = argparse.ArgumentParser(description='List values for a Numerous metric')
-parser.add_argument('--metric-id', required=True, help='Metric ID')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-m', '--metric-id', help='Metric ID')
+group.add_argument('-l', '--labelsearch', help='Metric label regular expression search')
 args = parser.parse_args()
 
-values = numerousapp.get_metric_values(args.metric_id);
-for value in values:
-    print datetime.datetime.strptime(value['updated'], '%Y-%m-%dT%H:%M:%S.%fZ')
-    print value['value']
-    print ""
+if args.labelsearch:
+  metric_ids = [ metric['id'] for metric in numerousapp.get_metrics(labelsearch = args.labelsearch) ]
+else:
+  metric_ids = [ args.metric_id ]
+
+for metric_id in metric_ids:
+  values = numerousapp.get_metric_values(metric_id = metric_id);
+  for value in values:
+      print datetime.datetime.strptime(value['updated'], '%Y-%m-%dT%H:%M:%S.%fZ')
+      print value['value']
+      print ""
 
 
