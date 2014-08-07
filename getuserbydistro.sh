1291b1d1584c1a8b19d826c7d067b03e1f8e7315
Ernie Hershey <ernie.hershey@10gen.com>
2014-08-06 23:40:30 -0400

    improve logic

--- getuserbydistro.sh
+++ getuserbydistro.sh
@@ -1,6 +1,11 @@
 getuserbydistro () 
 { 
     distro="$1";
-    egrep --no-filename ^$distro:\|user: ~/git/mci/config*/distros.yml* ~/distros.yml* | grep -A 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " "
+    user=$(egrep --no-filename ^$distro:\|user: ~/git/mci/config*/distros.yml* ~/distros.yml* | grep -A 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " ")
+    if [ ! "$user" ]
+    then
+      user=$(egrep --no-filename ^$distro:\|user: ~/git/mci/config*/distros.yml* ~/distros.yml* | grep -B 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " ")
+    fi
+    echo "$user"
 }
 
