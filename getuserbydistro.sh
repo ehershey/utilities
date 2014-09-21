434346799fa675834ee27a11082a8d0d3f0fa5c0
Ernie Hershey <ernie.hershey@10gen.com>
2014-09-21 11:21:54 -0400

    rework distros.yml paths

--- getuserbydistro.sh
+++ getuserbydistro.sh
@@ -1,10 +1,15 @@
 getuserbydistro () 
 { 
     distro="$1";
-    user=$(egrep --no-filename ^$distro:\|user: ~/git/mci/config*/distros.yml* ~/distros.yml* | grep -A 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " ")
+    if echo $distro | grep windows > /dev/null 2>&1
+    then
+      echo Administrator
+      return
+    fi
+    user=$(egrep --no-filename ^$distro:\|user: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | grep -A 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " ")
     if [ ! "$user" ]
     then
-      user=$(egrep --no-filename ^$distro:\|user: ~/git/mci/config*/distros.yml* ~/distros.yml* | grep -B 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " ")
+      user=$(egrep --no-filename ^$distro:\|user: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | grep -B 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " ")
     fi
     echo "$user"
 }
