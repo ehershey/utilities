a1a620fe357b0b312f681d48925e31c6676ce00e
Ernie Hershey <ernie.hershey@10gen.com>
2014-10-10 11:24:47 -0400

    Add hostname check

--- load_crontab_secondary.sh
+++ load_crontab_secondary.sh
@@ -4,12 +4,19 @@
 #
 CRON_USER=ernie
 FILEHOME=/home/ernie/git/utilities
+CRON_HOSTNAME=eahdroplet1
 
 if [ "$CRON_USER" != "$USER" ] 
 then
   command="sudo crontab -u $CRON_USER"
 else
   command="crontab"
+fi
+
+if [ "$CRON_HOSTNAME" != "$HOSTNAME" ]
+then
+  echo "Incorrect hostname: (looks like: $HOSTNAME, should be: $CRON_HOSTNAME)"
+  exit 3
 fi
 
 if [ ! -e "$FILEHOME/crontab.env" -o ! -e "$FILEHOME/crontab-secondary.txt" ] 
