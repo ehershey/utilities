50b5a30a97171e368b200c66c2c89e981a4a99dd
Ernie Hershey <ernie.hershey@10gen.com>
2014-10-20 19:40:54 -0400

    fix logic to only search a distros files

--- getuserbydistro.sh
+++ getuserbydistro.sh
@@ -6,10 +6,14 @@ getuserbydistro ()
       echo Administrator
       return
     fi
-    user=$(egrep --no-filename ^$distro:\|user: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | grep -A 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " ")
+    user=$(
+      grep -l ^$distro: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | xargs egrep --no-filename ^$distro:\|user: | grep -A 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " "
+    )
     if [ ! "$user" ]
     then
-      user=$(egrep --no-filename ^$distro:\|user: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | grep -B 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " ")
+      user=$(
+        grep -l ^$distro: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | xargs egrep --no-filename ^$distro:\|user: | grep -B 1 ^$distro: | head -1 | cut -f2 -d: | tr -d " "
+      )
     fi
     echo "$user"
 }
