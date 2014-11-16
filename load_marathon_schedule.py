ff5486f046294e95c4156137da5293867f2b59ba
Ernie Hershey <ernie.hershey@10gen.com>
2014-11-15 23:35:53 -0500

    work in progress

---
+++ load_marathon_schedule.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+# 
+# Given the date of a marathon, backfill a training schedule with training plan
+# culminating on the date of the marathon. 
+#
+# This depends on having a "training_plans" collection populated by the populating 
+# script, available here: 
+# https://github.com/ehershey/utilities/blob/master/nike_training_plan_to_mongodb.sh
+# 
+#
+import argparse
+from pymongo import MongoClient, DESCENDING
+
+DB = "ernie_org"
+TRAINING_PLANS_COLLECTION = "training_plan"
+TRAINING_SCHEDULES_COLLECTION = "training_schedule"
+
+EVENT_DISTANCE = 26.2
+
+parser = argparse.ArgumentParser(description='Load Marathon Training Schedule')
+parser.add_argument('marathon_date', help='Date of marathon')
+parser.add_argument('--level', help='Training Plan Level', type=int, choices=[1,2,3])
+args = parser.parse_args()
+
+client = MongoClient('localhost', 27017)
+db = client[DB]
+
+training_dates = db[TRAINING_PLANS_COLLECTION].find({ "level": args.level}).sort([ ("day", DESCENDING )])
+#training_dates = db[TRAINING_PLANS_COLLECTION].find()
+
+for training_date in training_dates:
+  print "training_date: %s" % training_date
+  distance = training_date['distance']
+  print "distance: %s" % distance
+
+
+
+#import datetime
+#import os
+#import os.path
+#import re
+#import time
+#from os.path import expanduser
+#from numerousapp import update_metric_value, get_metric_value
+#home = expanduser("~ernie")
+#
+#
+#today = datetime.datetime.now();
+#yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+#
+#yesterday_summary = db.nutrition_summary.find_one({ "date":  yesterday.strftime("%B %-d, %Y") });
+#
+#date_regex_2013 = re.compile(", 2013")
+#date_regex_2014 = re.compile(", 2014")
+#
+#nutrition_2013_average = db.nutrition_summary.aggregate([ { "$match": { "date": date_regex_2013 } }, { "$group": { "_id": "2013", "Average": { "$avg": "$calories_numeric" } } } ]);
+#nutrition_2014_average = db.nutrition_summary.aggregate([ { "$match": { "date": date_regex_2014 } }, { "$group": { "_id": "2014", "Average": { "$avg": "$calories_numeric" } } } ]);
+#
+#input_today_url = "#"
+#input_yesterday_url = "#"
+#input_today = 0
+#input_yesterday = 0
+#input_2013 = 0
+#input_today_2013_diff = 0
+#input_yesterday_2013_diff = 0
+#input_2014 = 0
+#input_today_2014_diff = 0
+#input_2014_2013_diff = 0
+#
+#if today_summary and today_summary['Calories']:
+#  input_today = round(today_summary['calories_numeric'], 2)
+#
+#if today_summary and today_summary['report_url']:
+#  input_today_url = today_summary['report_url']
+#
+#if yesterday_summary and yesterday_summary['Calories']:
+#  input_yesterday = round(yesterday_summary['calories_numeric'], 2)
+#
+#if yesterday_summary and yesterday_summary['report_url']:
+#  input_yesterday_url = yesterday_summary['report_url']
+#
+#
+#if nutrition_2013_average and nutrition_2013_average['result']:
+#  input_2013 = round(nutrition_2013_average['result'][0]['Average'], 2)
+#
+#if nutrition_2014_average and nutrition_2014_average['result']:
+#  input_2014 = round(nutrition_2014_average['result'][0]['Average'], 2)
+#
+#
+#input_today_2013_diff = round(input_2013 - input_today, 2)
+#input_yesterday_2013_diff = round(input_2013 - input_yesterday, 2)
+#input_today_2014_diff = round(input_2014 - input_today, 2)
+#input_2014_2013_diff = round(input_2013 - input_2014, 2)
+#
+#TEMPLATE_FILENAME = "%s/unit-report-template.html" % os.path.dirname(os.path.realpath(__file__))
+#MOVES_CSV_FILENAME = "%s/Dropbox/Web/moves.csv" % home
+#
+#MIN_HOUR_FOR_ZERO_POST = 5
+#
+#placeholder = {}
+#
+#units_today = os.popen("cut -f5 -d, %s  | head -2 | tail -1" % MOVES_CSV_FILENAME).read().rstrip()
+#biked_today = os.popen("cut -f2 -d, %s  | head -2 | tail -1 | tr -d a-z" % MOVES_CSV_FILENAME).read().rstrip()
+#ran_today = os.popen("cut -f3 -d, %s  | head -2 | tail -1 | tr -d a-z" % MOVES_CSV_FILENAME).read().rstrip()
+#walked_today = os.popen("cut -f4 -d, %s  | head -2 | tail -1 | tr -d a-z" % MOVES_CSV_FILENAME).read().rstrip()
+#units_average = os.popen("cut -f5 -d, %s| awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
+#units_yesterday = os.popen("cut -f5 -d, %s  | head -3 | tail -1" % MOVES_CSV_FILENAME).read().rstrip()
+#units_average_2013 = os.popen("grep ^2013- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
+#units_average_2014 = os.popen("grep ^2014- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
+#units_average_7days = os.popen("head -8 %s | tail -7 | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
+#units_average_2days = os.popen("head -3 %s | tail -2 | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
+#
+#
+#units_today_2013_diff = float(units_today) - float(units_average_2013)
+#units_yesterday_2013_diff = float(units_yesterday) - float(units_average_2013)
+#units_average_2013_diff = float(units_average) - float(units_average_2013)
+#units_average_7days_2013_diff = float(units_average_7days) - float(units_average_2013)
+#units_average_2days_2013_diff = float(units_average_2days) - float(units_average_2013)
+#units_average_2014_2013_diff = float(units_average_2014) - float(units_average_2013)
+#
+#minutes_since_moves_update = (datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getmtime(MOVES_CSV_FILENAME))).seconds / 60 
+#
+#if units_today_2013_diff > 0:
+#    placeholder['today_class'] = "positive_diff"
+#else:
+#    placeholder['today_class'] = "negative_diff"
+#
+#if input_today_2013_diff > 0:
+#    placeholder['input_class'] = "positive_diff"
+#else:
+#    placeholder['input_class'] = "negative_diff"
+#
+#if units_average_2days_2013_diff > 0:
+#    placeholder['2days_class'] = "positive_diff"
+#else:
+#    placeholder['2days_class'] = "negative_diff"
+#
+#if units_average_7days_2013_diff > 0:
+#    placeholder['7days_class'] = "positive_diff"
+#else:
+#    placeholder['7days_class'] = "negative_diff"
+#
+#if units_average_2014_2013_diff > 0:
+#    placeholder['2014_class'] = "positive_diff"
+#else:
+#    placeholder['2014_class'] = "negative_diff"
+#
+#if units_average_2013_diff > 0:
+#    placeholder['alltime_class'] = "positive_diff"
+#else:
+#    placeholder['alltime_class'] = "negative_diff"
+#
+#
+#placeholder['units_today_2013_diff'] = units_today_2013_diff
+#placeholder['units_today'] = units_today
+#placeholder['units_yesterday_2013_diff'] = units_yesterday_2013_diff
+#placeholder['units_yesterday'] = units_yesterday
+#placeholder['units_average'] = units_average
+#placeholder['units_average_2013_diff'] = units_average_2013_diff
+#placeholder['units_average_2013'] = units_average_2013
+#placeholder['units_average_2014'] = units_average_2014
+#placeholder['units_average_2014_2013_diff'] = units_average_2014_2013_diff
+#placeholder['units_average_7days'] = units_average_7days
+#placeholder['units_average_7days_2013_diff'] = units_average_7days_2013_diff
+#placeholder['units_average_2days'] = units_average_2days
+#placeholder['units_average_2days_2013_diff'] = units_average_2days_2013_diff
+#placeholder['now'] = time.ctime()
+#placeholder['moves_csv_modified'] = time.ctime(os.path.getmtime(MOVES_CSV_FILENAME))
+#placeholder['input_today'] = input_today
+#placeholder['input_yesterday'] = input_yesterday
+#placeholder['input_2013'] = input_2013
+#placeholder['input_today_2013_diff'] = input_today_2013_diff
+#placeholder['input_yesterday_2013_diff'] = input_yesterday_2013_diff
+#placeholder['input_2014'] = input_2014
+#placeholder['input_today_2014_diff'] = input_today_2014_diff
+#placeholder['input_2014_2013_diff'] = input_2014_2013_diff
+#placeholder['input_today_url'] = input_today_url
+#placeholder['input_yesterday_url'] = input_yesterday_url
+#
+#
+## echo "units_today: $units_today"
+## echo "units_average: $units_average"
+## echo "units_average_2013: $units_average_2013"
+## echo "units_average_2014: $units_average_2014"
+## echo "units_average_7days: $units_average_7days"
+#
+#
+#
+#if __name__ == '__main__':
+#
+#    with open (TEMPLATE_FILENAME, "r") as myfile:
+#          template=myfile.read()
+#
+#    formated = template.format(**placeholder)
+#    print formated
+#
+#    # Report to numerous
+#    #
+#    # TODO: move this out of report script
+#    #
+#
+#    # Only post if we have data for today or it's been long enough that we should have data for today
+#    #
+#    if units_today > 0 or datetime.datetime.now().hour > MIN_HOUR_FOR_ZERO_POST:
+#      running_goal = get_metric_value(5818738672989877729)
+#      walking_goal = get_metric_value(7758304728227275179)
+#      biking_goal = get_metric_value(5595761423440323786)
+#      if ran_today == "" or ran_today == None:
+#          ran_today = 0
+#      if biked_today == "" or biked_today == None:
+#          biked_today = 0
+#      if walked_today == "" or walked_today == None:
+#          walked_today = 0
+#      if running_goal:
+#        running_goal = running_goal['value']
+#      else:
+#        running_goal = 0
+#      if walking_goal:
+#        walking_goal = walking_goal['value']
+#      else:
+#        walking_goal = 0
+#      if biking_goal:
+#        biking_goal = biking_goal['value']
+#      else:
+#        biking_goal = 0
+#
+#      biked_today = float(biked_today)
+#      ran_today = float(ran_today)
+#      walked_today = float(walked_today)
+#      update_metric_value(8748753640388107935, running_goal - ran_today)
+#      update_metric_value(94526752002396912, walking_goal - walked_today)
+#      update_metric_value(111285933456617558, biking_goal - biked_today)
+#      #update_metric_value(6359390767342201980, units_yesterday, updated = "2014-06-15T23:59:59.000Z")
+#      update_metric_value(6359390767342201980, units_today)
+#      update_metric_value(7670190745339240677, biked_today)
+#      update_metric_value(3043034116976301897, walked_today)
+#      update_metric_value(3398081990673437243, ran_today)
+#      update_metric_value(5212351794073589044, units_today_2013_diff)
+#      update_metric_value(7170780739467042866, units_average_2days)
+#      update_metric_value(1242812163656294116, units_average_2days_2013_diff)
+#      update_metric_value(3766962275739755372, minutes_since_moves_update)
