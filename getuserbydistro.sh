c684bc59ce82c070c4f0dcb0474ac8325e4b8ed0
Ernie Hershey <ernie.hershey@10gen.com>
2014-11-19 11:03:31 -0500

    clean up greps

--- getuserbydistro.sh
+++ getuserbydistro.sh
@@ -7,12 +7,12 @@ getuserbydistro ()
       return
     fi
     user=$(
-      grep -l ^$distro: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | xargs egrep --no-filename ^$distro:\|user: | grep -A 1 ^$distro: | tail -1 | cut -f2 -d: | tr -d " "
-    )
+      grep -l ^$distro: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | xargs egrep --no-filename ^$distro:\|user: | grep -A 1 ^$distro: | grep -v ^$distro: | grep -v -- --  | tail -1 | cut -f2 -d: | tr -d " "
+   )
     if [ ! "$user" ]
     then
       user=$(
-        grep -l ^$distro: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | xargs egrep --no-filename ^$distro:\|user: | grep -B 1 ^$distro: | head -1 | cut -f2 -d: | tr -d " "
+        grep -l ^$distro: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | xargs egrep --no-filename ^$distro:\|user: | grep -B 1 ^$distro: | grep -v ^$distro: | grep -v -- -- | head -1 | cut -f2 -d: | tr -d " "
       )
     fi
     echo "$user"
