01fae8e560b16a559f478d3ad59bbecef873f8a7
Ernie Hershey <ernie.hershey@10gen.com>
2014-12-10 17:22:41 -0500

    turn on bluetooth when iphone connected

---
+++ monitor_iphone.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#
+# Turn bluetooth on when my iphone is connected via usb
+#
+# Requires Blueutil (https://github.com/toy/blueutil)
+#
+
+export "PATH=/usr/sbin:/usr/local/bin:$PATH"
+
+# To populate $iphone_serial
+#
+. $(dirname "$0")/monitor_iphone_env.sh
+
+echo "iphone_serial: $iphone_serial"
+
+found=$(system_profiler SPUSBDataType -xml | xpath "//string[contains(text(),'$iphone_serial')][preceding-sibling::*[1][contains(text(),'serial_num')]]" 2>&1 | grep $iphone_serial)
+
+echo "usb output: $found"
+
+if [ "$found" ]
+then
+  echo "blueutil power 1"
+  blueutil power 1
+fi
