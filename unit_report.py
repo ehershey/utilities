a0ea6e5f6b59ebd8bc67bb720bd2b32f35a212b4
Ernie Hershey <github@ernie.org>
2015-01-14 20:44:36 -0500

    Add 2015 average surplus

--- unit_report.py
+++ unit_report.py
@@ -27,6 +27,7 @@ date_regex_2015 = re.compile(", 2015")
 
 nutrition_2013_average = db.nutrition_summary.aggregate([ { "$match": { "date": date_regex_2013 } }, { "$group": { "_id": "2013", "Average": { "$avg": "$calories_numeric" } } } ]);
 nutrition_2014_average = db.nutrition_summary.aggregate([ { "$match": { "date": date_regex_2014 } }, { "$group": { "_id": "2014", "Average": { "$avg": "$calories_numeric" } } } ]);
+nutrition_2015_average = db.nutrition_summary.aggregate([ { "$match": { "date": date_regex_2015 } }, { "$group": { "_id": "2015", "Average": { "$avg": "$calories_numeric" } } } ]);
 nutrition_2015_total = db.nutrition_summary.aggregate([ { "$match": { "date": date_regex_2015 } }, { "$group": { "_id": "2015", "Total": { "$sum": "$calories_numeric" } } } ]);
 
 input_today_url = "#"
@@ -47,6 +48,7 @@ surplus_2014 = 0
 surplus_today_2014_diff = 0
 surplus_yesterday_2014_diff = 0
 surplus_2015_total = 0
+input_2015 = 0
 input_2015_total = 0
 
 if today_summary and today_summary['Calories']:
@@ -67,6 +69,9 @@ if nutrition_2013_average and nutrition_2013_average['result']:
 
 if nutrition_2014_average and nutrition_2014_average['result']:
   input_2014 = round(nutrition_2014_average['result'][0]['Average'], 2)
+
+if nutrition_2015_average and nutrition_2015_average['result']:
+  input_2015 = round(nutrition_2015_average['result'][0]['Average'], 2)
 
 if nutrition_2015_total and nutrition_2015_total['result']:
   input_2015_total = round(nutrition_2015_total['result'][0]['Total'], 2)
@@ -95,6 +100,7 @@ units_average = os.popen("cut -f5 -d, %s| awk '{ total += $1; count++ } END { pr
 units_yesterday = os.popen("cut -f5 -d, %s  | head -3 | tail -1" % MOVES_CSV_FILENAME).read().rstrip()
 units_average_2013 = os.popen("grep ^2013- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
 units_average_2014 = os.popen("grep ^2014- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
+units_average_2015 = os.popen("grep ^2015- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
 units_2015_total = os.popen("grep ^2015- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print total }'" % MOVES_CSV_FILENAME).read().rstrip()
 day_count_2015 = os.popen("grep ^2015- %s | cut -f5 -d, | awk '{ total += $1; count++ } END { print count }'" % MOVES_CSV_FILENAME).read().rstrip()
 units_average_7days = os.popen("head -8 %s | tail -7 | cut -f5 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
@@ -103,6 +109,7 @@ units_average_2days = os.popen("head -3 %s | tail -2 | cut -f5 -d, | awk '{ tota
 surplus_today = float(input_today) - (float(units_today) + resting_daily_calories)
 surplus_yesterday = float(input_yesterday) - (float(units_yesterday) + resting_daily_calories)
 surplus_2014 = float(input_2014) - (float(units_average_2014) + resting_daily_calories)
+surplus_2015 = float(input_2015) - (float(units_average_2015) + resting_daily_calories)
 surplus_2015_total = float(input_2015_total) - (float(units_2015_total) + resting_daily_calories * int(day_count_2015))
 
 surplus_today_2014_diff = round(surplus_2014 - surplus_today, 2)
@@ -193,6 +200,7 @@ placeholder['input_today_url'] = input_today_url
 placeholder['input_yesterday_url'] = input_yesterday_url
 placeholder['surplus_today'] = surplus_today
 placeholder['surplus_2014'] = surplus_2014
+placeholder['surplus_2015'] = surplus_2015
 placeholder['surplus_yesterday'] = surplus_yesterday
 placeholder['surplus_today_2014_diff'] = surplus_today_2014_diff
 placeholder['surplus_yesterday_2014_diff'] = surplus_yesterday_2014_diff
