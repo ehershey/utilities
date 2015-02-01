6f8265a46d9dfa56ff276ca1d15fea1dad60cace
Ernie Hershey <github@ernie.org>
2015-01-31 23:55:43 -0500

    Fix weird data from Nike

--- nike_training_plan_to_mongodb.sh
+++ nike_training_plan_to_mongodb.sh
@@ -12,5 +12,5 @@ echo "db.$COLLECTION.ensureIndex({level: 1, day: 1}, { unique: true });" | mongo
 for level in 1 2 3
 do
 
-  (echo day,distance,notes,level; cache_get 86400  "http://nikerunning.nike.com/nikeplus/v2/schedules/plus/marathon_${level}_en_us.xml" | sed 's/<\/Day>/#/g' | tr -d \\n | tr \# \\n | sed 's/ *<Day> */0/g' | sed "s/.*PlannedDistance value='//g" | sed "s/'.*='/#/g" | sed "s/'.*//g" | tr -d \\t | tr \# ,  | sed 's/\([0-9]\)$/\1,/' | grep -v ^\< | sed s/$/,$level/ | grep -n  '' | tr : ,    )|mongoimport --db "$DB" --collection "$COLLECTION" --type csv --headerline --upsertFields day,level
+  (echo day,distance,notes,level; cache_get 86400  "http://nikerunning.nike.com/nikeplus/v2/schedules/plus/marathon_${level}_en_us.xml" | sed 's/<\/Day>/#/g' | tr -d \\n | tr \# \\n | sed 's/ *<Day> */0/g' | sed "s/.*PlannedDistance value='//g" | sed 's/<Activity.*//' | sed "s/'.*='/#/g" | sed "s/'.*//g" | tr -d \\t | tr \# ,  | sed 's/\([0-9]\)$/\1,/' | grep -v ^\< | sed s/$/,$level/ | grep -n  '' | tr : ,    )|mongoimport --db "$DB" --collection "$COLLECTION" --type csv --headerline --upsertFields day,level
 done
