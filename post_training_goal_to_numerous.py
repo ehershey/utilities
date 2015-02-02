86112529cb1267332e3e6ff3838dddf525edc5bd
Ernie Hershey <github@ernie.org>
2015-02-01 23:37:46 -0500

    debug logging

--- post_training_goal_to_numerous.py
+++ post_training_goal_to_numerous.py
@@ -18,22 +18,27 @@ METRIC_ID = 5818738672989877729
 
 def main():
 
-  today = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+  today = datetime.datetime.now().replace(hour=12, minute=0, second=0, microsecond=0)
   twelve_hours_ago = today - datetime.timedelta(hours=12)
   twelve_hours_from_now = today + datetime.timedelta(hours=12)
+
+  print "twelve_hours_ago: " + str(twelve_hours_ago)
+  print "twelve_hours_from_now: " + str(twelve_hours_from_now)
 
   client = MongoClient('localhost', 27017)
   db = client[DB]
 
   schedule_collection = db[TRAINING_SCHEDULES_COLLECTION]
   schedule_workout = schedule_collection.find_one({ "date": {"$gte": twelve_hours_ago, "$lt": twelve_hours_from_now}})
-  if schedule_workout is None:
+  if schedule_workout:
+    goal_distance = schedule_workout['distance']
+  else:
     print "No workout found"
-    exit()
-
+    print "Using 0 distance"
+    goal_distance = 0
   print "schedule_workout: %s" % schedule_workout
-  print "Setting running goal to: %s" % schedule_workout['distance']
-  update_metric_value(METRIC_ID, schedule_workout['distance'])
+  print "Setting running goal to: %s" % goal_distance
+  update_metric_value(METRIC_ID, goal_distance)
 
 
 if __name__ == '__main__':
