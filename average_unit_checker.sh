0ad819205f3ddef0a0fb9e551e24aebc44fe4413
Ernie Hershey <github@ernie.org>
2015-03-20 11:56:54 -0400

    Comments and change 'unit' to 'calorie'

--- average_unit_checker.sh
+++ average_unit_checker.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
+#
+# Email when daily calorie expenditure passes average and double average
+#
 
 export calories_column=$(head -1 ~/Dropbox/Web/moves.csv  | tr , \\n  | grep -n Calories | cut -f1 -d:)
 echo "calories_column: $calories_column"
 
-units_average=$(cut -f$calories_column -d, ~/Dropbox/Web/moves.csv  | awk '{ total += $1; count++ } END { print total/count }' | sed 's/\..*//')
-double_units_average=$(cut -f$calories_column -d, ~/Dropbox/Web/moves.csv  | awk '{ total += 2*$1; count++ } END { print total/count }' | sed 's/\..*//')
-units_today=$(cut -f$calories_column -d, ~/Dropbox/Web/moves.csv  | head -2 | tail -1 | sed 's/\..*//')
+calories_average=$(cut -f$calories_column -d, ~/Dropbox/Web/moves.csv  | awk '{ total += $1; count++ } END { print total/count }' | sed 's/\..*//')
+double_calories_average=$(cut -f$calories_column -d, ~/Dropbox/Web/moves.csv  | awk '{ total += 2*$1; count++ } END { print total/count }' | sed 's/\..*//')
+calories_today=$(cut -f$calories_column -d, ~/Dropbox/Web/moves.csv  | head -2 | tail -1 | sed 's/\..*//')
 
-LOCKFILE=/tmp/mailed_unit_message.lock
-DOUBLE_LOCKFILE=/tmp/mailed_double_unit_message.lock
+LOCKFILE=/tmp/mailed_calorie_message.lock
+DOUBLE_LOCKFILE=/tmp/mailed_double_calorie_message.lock
 
-echo "units_today: $units_today"
-echo "units_average: $units_average"
-echo "double_units_average: $double_units_average"
-if [ "$units_today" -gt "$units_average" ]
+echo "calories_today: $calories_today"
+echo "calories_average: $calories_average"
+echo "double_calories_average: $double_calories_average"
+if [ "$calories_today" -gt "$calories_average" ]
 then
   if [ ! -e "$LOCKFILE" ]
   then
-    echo "Surpassed unit average! (today: $units_today, average: $units_average)" | mail average_unit_checker@ernie.org
+    echo "Surpassed calorie average! (today: $calories_today, average: $calories_average)" | mail average_calorie_checker@ernie.org
     touch "$LOCKFILE"
   fi
 else
   rm -rf "$LOCKFILE"
 fi
 
-if [ "$units_today" -gt "$double_units_average" ]
+if [ "$calories_today" -gt "$double_calories_average" ]
 then
   if [ ! -e "$DOUBLE_LOCKFILE" ]
   then
-    echo "Surpassed DOUBLE unit average! (today: $units_today, average: $units_average)" | mail average_unit_checker@ernie.org
+    echo "Surpassed DOUBLE calorie average! (today: $calories_today, average: $calories_average)" | mail average_calorie_checker@ernie.org
     touch "$DOUBLE_LOCKFILE"
   fi
 else
