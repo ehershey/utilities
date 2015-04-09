ae57f74c9e4637eb43de6e86a29fc921d6a1c0da
Ernie Hershey <github@ernie.org>
2015-04-09 11:37:45 -0400

    Search dropbox bash history

---
+++ global_history.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CACHE_TIME=300
+DIR=~/Dropbox/Misc/bash_history
+tempfile=$(mktemp /tmp/global_history.XXXXXX)
+tempfile2=$(mktemp /tmp/global_history.XXXXXX)
+
+
+cache_run 300 "ls -tr \"$DIR\"/*" | xargs cat > "$tempfile"
+while [ "$1" ]
+do
+  cat "$tempfile" | grep -i "$1" > "$tempfile2"
+  cat "$tempfile2" > "$tempfile"
+  shift
+done
+
+cat $tempfile
+rm "$tempfile" "$tempfile2"
