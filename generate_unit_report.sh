b3fb12b1745cd0c82aba1260d3f8342c423d3df3
Ernie Hershey <github@ernie.org>
2015-04-15 15:39:34 -0400

    Support osx md5 executable

--- generate_unit_report.sh
+++ generate_unit_report.sh
@@ -6,12 +6,24 @@ SCRIPT=~ernie/git/utilities/unit_report.py
 REPORT=~ernie/Dropbox/Web/goeverywhere/unit_report.html
 
 
+if which md5 &>/dev/null
+then
+  md5=md5
+elif which md5sum &> /dev/null
+then
+  md5=md5sum
+else
+  # don't use checksum for cache file
+  #
+  md5=cat
+fi
+
 tempfile=$(mktemp /tmp/average_unit_report.XXXXXX)
 
 $SCRIPT > "$tempfile"
 
-old_md5=$(grep -v diffignore "$REPORT" | md5sum)
-new_md5=$(grep -v diffignore "$tempfile" | md5sum)
+old_md5=$(grep -v diffignore "$REPORT" | $md5)
+new_md5=$(grep -v diffignore "$tempfile" | $md5)
 
 echo "old_md5: $old_md5"
 echo "new_md5: $new_md5"
