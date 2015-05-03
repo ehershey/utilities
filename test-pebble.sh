211ab7a24bfa098f5294414e9076146ce3b2f1f5
Ernie Hershey <github@ernie.org>
2015-05-03 05:03:26 -0400

    whitespace cleanup

--- test-pebble.sh
+++ test-pebble.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-i=2000 
+i=2000
 if [ "$1" ]
 then
   export recip="$1"
 else
   export recip=pebble@ernie.org
 fi
-while true 
-do 
+while true
+do
    date=`date`
- echo $i 
+ echo $i
  echo $date
- (echo test $i 
+ (echo test $i
  date ) | mail -s "test $i at $date" $recip
-  let i=i+1 
- sleep 5 
+  let i=i+1
+ sleep 5
 done
