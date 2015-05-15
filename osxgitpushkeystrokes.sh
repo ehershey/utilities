b2d40b8686068744d77f813a89517e1b766c775e
Ernie Hershey <github@ernie.org>
2015-05-15 14:28:13 -0400

    Move branch to variable

--- osxgitpushkeystrokes.sh
+++ osxgitpushkeystrokes.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+# TODO - eat multiple event triggers
+#
+BRANCH=v3.0
 date >> /tmp/button.log
-osascript -e 'tell application "System Events" to keystroke "echo git push origin master\n"'
+osascript -e 'tell application "System Events" to keystroke "git push origin $BRANCH\n"'
 date >> /tmp/button.log
