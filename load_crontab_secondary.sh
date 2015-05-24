10cb5600ea1f79b0f5709380553ce6ef9b16c15b
Ernie Hershey <github@ernie.org>
2015-05-24 00:56:40 -0400

    whitespace #2

--- load_crontab_secondary.sh
+++ load_crontab_secondary.sh
@@ -6,7 +6,7 @@ CRON_USER=ernie
 FILEHOME=/home/ernie/git/utilities
 CRON_HOSTNAME=eahdroplet1
 
-if [ "$CRON_USER" != "$USER" ] 
+if [ "$CRON_USER" != "$USER" ]
 then
   command="sudo crontab -u $CRON_USER"
 else
@@ -19,9 +19,9 @@ then
   exit 3
 fi
 
-if [ ! -e "$FILEHOME/crontab.env" -o ! -e "$FILEHOME/crontab-secondary.txt" ] 
+if [ ! -e "$FILEHOME/crontab.env" -o ! -e "$FILEHOME/crontab-secondary.txt" ]
 then
   echo "Can't find crontab.env or crontab-secondary.txt in $FILEHOME"
   exit 2
 fi
-cat "$FILEHOME/crontab.env" "$FILEHOME/crontab-secondary.txt" | $command 
+cat "$FILEHOME/crontab.env" "$FILEHOME/crontab-secondary.txt" | $command
