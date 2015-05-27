eaacd7cbeb405821fc766af284aad4b03b821ad5
Ernie Hershey <github@ernie.org>
2015-05-27 08:45:24 -0400

    Add color if stdout is a tty

--- build_status.py
+++ build_status.py
@@ -1,42 +1,64 @@
 #!/usr/bin/python
 
 import requests
+import sys
 
 URL = 'https://evergreen.mongodb.com/json/timeline/mongodb-mongo-master?limit=1&skip=0'
 
 response = requests.get(URL)
 data = response.json()
 
-print "Build Variant,Status, Total, Success Count, Failed count"
-for version in data['Versions']:
-    builds = version['Builds']
-    for build in builds:
-        line=""
-        build = build['Build']
-        line += build['display_name']
-        line += ","
-        build_status = ''
-        tasks = build['tasks']
-        success_count=0
-        failed_count=0
-        tasks = build['tasks']
-        task_count = len(tasks)
-        for task in tasks:
-            if task['status'] == 'success':
-                success_count =                 success_count + 1
-            elif task['status'] == 'failed':
-                failed_count =                 failed_count + 1
-        if failed_count > 0:
-            build_status = 'failed'
-        elif success_count == task_count:
-            build_status = 'success'
-        else:
-            build_status = 'incomplete'
-        line += build_status
-        line += ','
-        line += str(task_count)
-        line += ','
-        line += str(success_count)
-        line += ','
-        line += str(failed_count)
-        print line
+def main():
+    colorprint(bcolors.BOLD, "Build Variant,Status, Total, Success Count, Failed count")
+    for version in data['Versions']:
+        builds = version['Builds']
+        for build in builds:
+            line=""
+            build = build['Build']
+            line += build['display_name']
+            line += ","
+            build_status = ''
+            tasks = build['tasks']
+            success_count=0
+            failed_count=0
+            tasks = build['tasks']
+            task_count = len(tasks)
+            for task in tasks:
+                if task['status'] == 'success':
+                    success_count = success_count + 1
+                elif task['status'] == 'failed':
+                    failed_count = failed_count + 1
+            line_start = None
+            if failed_count > 0:
+                build_status = 'failed'
+                if sys.stdout.isatty():
+                    line_start = bcolors.RED
+            elif success_count == task_count:
+                build_status = 'success'
+                if sys.stdout.isatty():
+                    line_start = bcolors.GREEN
+            else:
+                build_status = 'incomplete'
+            line += build_status
+            line += ','
+            line += str(task_count)
+            line += ','
+            line += str(success_count)
+            line += ','
+            line += str(failed_count)
+            colorprint(line_start, line)
+
+class bcolors:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+def colorprint(color, text):
+    if color and sys.stdout.isatty():
+        sys.stdout.write(color)
+    print(text)
+    if color and sys.stdout.isatty():
+        sys.stdout.write(bcolors.ENDC)
+
+main()
