7755dc213514683ac0185e24dc2ec1620181d93a
Ernie Hershey <github@ernie.org>
2015-06-18 23:55:12 -0500

    whitespace

--- restart_mongod.sh
+++ restart_mongod.sh
@@ -2,7 +2,7 @@
 
 # restart mongod service if it's not running
 #
-if ! /sbin/service mongod status 
-then 
-  /sbin/service mongod start 
+if ! /sbin/service mongod status
+then
+  /sbin/service mongod start
 fi
