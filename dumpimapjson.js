cce022459c39a57f6f843444ec82112b101fd313
Ernie Hershey <github@ernie.org>
2015-07-12 23:49:26 -0400

    Change date, fetch one message at a time

--- dumpimapjson.js
+++ dumpimapjson.js
@@ -15,7 +15,7 @@ var Imap = require('imap');
 
 //var since = 'January 01, 2013';
 //var folder = 'UNSEEN';
-var since = 'January 01, 2010';
+var since = 'January 01, 2015';
 var folder = 'ALL';
 
 var authdata = require('authdata');
@@ -40,7 +40,8 @@ function die(err) {
 }
 
 function openInbox(cb) {
-  imap.openBox('INBOX', true, cb);
+  // imap.openBox('INBOX', true, cb);
+  imap.openBox('[Gmail]/All Mail', true, cb);
 }
 
 imap.once('ready', function() {
@@ -50,44 +51,59 @@ imap.once('ready', function() {
     //imap.search([ folder, ['SINCE', since] ], function(err, results) {
     imap.search([ folder, ['SINCE', since] ], function(err, results) {
       if (err) die(err);
+      process.stderr.write('Got search results\n');
       var index = 0;
-      var f = imap.fetch(results, { headers: true, bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)', struct: true });
-      f.on('message', function(msg, seqno) {
-        index++;
-        var this_index = index;
-        var message_output = {msg: msg, seqno: seqno, index: this_index};
-        process.stderr.write('.');
-        // process.stderr.write('starting message no. ' + this_index + "\n");
-        msg.on('body', function(stream, info) {
-          var buffer = '';
-          stream.on('data', function(chunk) {
-            buffer += chunk.toString('utf8');
-          });
-          stream.once('end', function() {
-            // process.stderr.write('buffer: ' + Imap.parseHeader(buffer)); // 'Parsed header: %s', util.inspect(Imap.parseHeader(buffer)));
-            message_output.headers = Imap.parseHeader(buffer);
-          });
-        });
-        msg.once('attributes', function(attrs) {
-          // console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-          message_output.attrs = attrs;
-          // process.stderr.write('Headers for no. ' + seqno + ': ' + show(attrs) + "\n");
-        });
-        msg.once('end', function() {
-          // process.stderr.write('ending message no. ' + this_index + "\n");
+      for(var i = 0 ; i < results.length ; i++)
+      {
+        process.stderr.write('<');
+        var f = imap.fetch(results[i], { headers: true, bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)', struct: true });
+        f.on('message', function(msg, seqno) {
+          index++;
+          var this_index = index;
+          var message_output = {"_id": results[i], msg: msg, seqno: seqno, index: this_index};
           process.stderr.write('.');
-          process.stdout.write(JSON.stringify(message_output));
-          process.stdout.write("\n");
+          process.stderr.write(results[i]);
+          // process.stderr.write('starting message no. ' + this_index + "\n");
+          msg.on('body', function(stream, info) {
+            var buffer = '';
+            stream.on('data', function(chunk) {
+              buffer += chunk.toString('utf8');
+            });
+            stream.once('end', function() {
+              // process.stderr.write('buffer: ' + Imap.parseHeader(buffer)); // 'Parsed header: %s', util.inspect(Imap.parseHeader(buffer)));
+              message_output.headers = Imap.parseHeader(buffer);
+            });
+          });
+          msg.once('attributes', function(attrs) {
+            // console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+            message_output.attrs = attrs;
+            // process.stderr.write('Headers for no. ' + seqno + ': ' + show(attrs) + "\n");
+          });
+          msg.once('end', function() {
+            // process.stderr.write('ending message no. ' + this_index + "\n");
+            process.stderr.write('.');
+            process.stdout.write(JSON.stringify(message_output));
+            process.stdout.write("\n");
+          });
         });
-      });
-      f.once('error', function(err) {
-          if (err) die(err);
-      });
-      f.once('end',function() {
-          process.stderr.write('\nDone fetching all messages!' + "\n");
-          imap.end();
-          process.exit();
-      });
+        f.once('error', function(err) {
+            if (err) die(err);
+        });
+        var seen_ends = 0;
+        f.once('end',function() {
+            seen_ends++;
+            if(seen_ends == results.length)
+            {
+              process.stderr.write('\nDone fetching all messages!' + "\n");
+              imap.end();
+              process.exit();
+            }
+            else
+            {
+              process.stderr.write('>');
+            }
+        });
+      }
     });
   });
 });
