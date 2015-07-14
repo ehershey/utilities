7adca0873cf5015936dd5948064e385cba5f7aa8
Ernie Hershey <github@ernie.org>
2015-07-14 09:41:15 -0400

    Add --metric-id parameter

--- list_numerous_metrics.py
+++ list_numerous_metrics.py
@@ -9,13 +9,19 @@ import sys
 
 
 parser = argparse.ArgumentParser(description='List Numerous metrics')
-parser.add_argument('-l', '--labelsearch', help='Label regular expression search', default='')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-l', '--labelsearch', help='Label regular expression search', default='')
+group.add_argument('-m', '--metric-id', help='Metric ID')
 parser.add_argument('-v', '--verbose', action='store_true', help='Include some extra details')
 parser.add_argument('-d', '--debug', action='store_true', help='Include a lot of extra details')
 args = parser.parse_args()
 
 
-metrics = numerousapp.get_metrics(args.labelsearch)
+if args.labelsearch:
+  metrics = numerousapp.get_metrics(labelsearch = args.labelsearch)
+else:
+  metrics = [ numerousapp.get_metric(metric_id = args.metric_id) ]
+
 
 for metric in metrics:
     # print metric
