43981257b7ceb588a451c20b834882e35f5227dd
Ernie Hershey <github@ernie.org>
2015-08-20 23:53:35 -0400

    attempt to support filter command line parameter

--- list_tasks.py
+++ list_tasks.py
@@ -9,10 +9,19 @@
 
 from pytodoist import todoist
 import todoist_auth
+import argparse
+DEFAULT_FILTER = "overdue, today, & p:Inbox"
+
+parser = argparse.ArgumentParser(description='List Todoist Tasks')
+parser.add_argument('-f', '--filter', help='Filter to search with', default = DEFAULT_FILTER)
+parser.add_argument('-v', '--verbose', action='store_true', help='Include some extra details')
+args = parser.parse_args()
 
 user = todoist.login(email = todoist_auth.email, password = todoist_auth.password)
 
-tasks = user.search_tasks("overdue, today, & p:Inbox")
+if args.verbose:
+    print "Filter: {0}".format(args.filter)
+tasks = user.search_tasks(args.filter)
 
 for task in tasks:
   print task.content
