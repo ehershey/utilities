898478c0c55dd9f57cd13db1255b9cb676b58c2b
Ernie Hershey <github@ernie.org>
2015-08-21 21:12:23 -0400

    print timestamp from yesterday

---
+++ yesterday.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#
+# Print the timestamp 24 hours ago
+#
+#
+import datetime
+now = datetime.datetime.now()
+yesterday = now + datetime.timedelta(days = -1)
+print datetime.datetime.strftime(now,"%+")
