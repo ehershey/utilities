357f21c7ca115f05da199a65f3aedd2b5dc887de
Ernie Hershey <github@ernie.org>
2015-08-26 15:40:21 -0400

    work in progress

---
+++ task_complete.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+"""
+
+Mark a Todoist task complete
+
+See also:
+http://www.todoist.com/
+
+"""
+
+from pytodoist import todoist
+import todoist_auth
+import argparse
+DEFAULT_FILTER = "overdue, today, & p:Inbox"
+
+parser = argparse.ArgumentParser(description='Mark Todoist Task complete')
+parser.add_argument('-f', '--filter', help='Filter to search with',
+                    default=DEFAULT_FILTER)
+parser.add_argument('-e', '--exclude_projects',
+                    help='Comma separated list of projects whose tasks \
+                          to exclude from output')
+parser.add_argument('-v', '--verbose', action='store_true', help='Include some extra details')
+args = parser.parse_args()
+
+exclude_projects = args.exclude_projects.lower().split(",")
+
+user = todoist.login(email = todoist_auth.email, password = todoist_auth.password)
+
+if args.verbose:
+    print "Filter: {0}".format(args.filter)
+tasks = user.search_tasks(args.filter)
+
+for task in tasks:
+    if task.project and task.project.name and \
+       task.project.name.lower() in exclude_projects:
+       continue
+    if args.verbose:
+        print "project: {0}".format(task.project.name)
+    print task.content
