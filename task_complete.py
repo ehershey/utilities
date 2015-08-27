bc87fb5012a6716d24208cbd4c7cb276f1c174c4
Ernie Hershey <github@ernie.org>
2015-08-27 14:37:05 -0400

    working script

--- task_complete.py
+++ task_complete.py
@@ -11,29 +11,41 @@ http://www.todoist.com/
 from pytodoist import todoist
 import todoist_auth
 import argparse
+
+
+def complete_task(task):
+    print "Marking task complete: " + task.content
+    task.complete()
+
 DEFAULT_FILTER = "overdue, today, & p:Inbox"
 
 parser = argparse.ArgumentParser(description='Mark Todoist Task complete')
-parser.add_argument('-f', '--filter', help='Filter to search with',
-                    default=DEFAULT_FILTER)
-parser.add_argument('-e', '--exclude_projects',
-                    help='Comma separated list of projects whose tasks \
-                          to exclude from output')
+parser.add_argument('text_to_match', help='Task text')
+parser.add_argument('--exact_match_only', action='store_true', help='If task text must be exact (default is substring match)')
 parser.add_argument('-v', '--verbose', action='store_true', help='Include some extra details')
 args = parser.parse_args()
 
-exclude_projects = args.exclude_projects.lower().split(",")
 
-user = todoist.login(email = todoist_auth.email, password = todoist_auth.password)
+user = todoist.login(email=todoist_auth.email, password=todoist_auth.password)
 
-if args.verbose:
-    print "Filter: {0}".format(args.filter)
-tasks = user.search_tasks(args.filter)
+tasks = user.search_tasks(DEFAULT_FILTER)
+
+matching_tasks = []
+exact_matching_tasks = []
+
+text_to_match = args.text_to_match.lower()
 
 for task in tasks:
-    if task.project and task.project.name and \
-       task.project.name.lower() in exclude_projects:
-       continue
-    if args.verbose:
-        print "project: {0}".format(task.project.name)
-    print task.content
+    task_content = task.content.lower()
+    if text_to_match in task_content:
+        matching_tasks.append(task)
+    if text_to_match == task_content:
+        exact_matching_tasks.append(task)
+
+if len(matching_tasks) == 1 and not args.exact_match_only:
+    complete_task(matching_tasks[0])
+else:
+    if len(exact_matching_tasks) == 1:
+        complete_task(exact_matching_tasks[0])
+    else:
+        print "Invalid length of list of matching tasks: " + str(len(matching_tasks))
