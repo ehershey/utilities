4705518f396ba0919fe40496135deb11df0f7f74
Ernie Hershey <github@ernie.org>
2015-09-01 17:33:12 -0400

    pep8 fixes

--- add_task.py
+++ add_task.py
@@ -12,17 +12,16 @@
 import argparse
 from pytodoist import todoist
 import todoist_auth
-import datetime
 
 parser = argparse.ArgumentParser(description='Test packages')
-parser.add_argument('--title', help='Task Title', default = None, required = True);
-parser.add_argument('--due', help='Due Date', default = "today");
+parser.add_argument('--title', help='Task Title', default=None, required=True)
+parser.add_argument('--due', help='Due Date', default="today")
 args = parser.parse_args()
 
-user = todoist.login(email = todoist_auth.email, password = todoist_auth.password)
+user = todoist.login(email=todoist_auth.email, password=todoist_auth.password)
 
 inbox = user.get_project('Inbox')
 
 content = "Test Content"
 
-inbox.add_task(args.title, date = args.due)
+inbox.add_task(args.title, date=args.due)
