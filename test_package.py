208d97429f91874f5d1c3683de0b90d40176499e
Ernie Hershey <github@ernie.org>
2015-09-05 08:27:45 -0400

    pep8 fixes

--- test_package.py
+++ test_package.py
@@ -2,7 +2,8 @@
 import boto.ec2
 import datetime
 from fabric.api import execute, env
-from fabfile import  setuprepo, installpackage, verifypackage, uninstallpackage, start_mongod, stop_mongod, verifymongodrunning, verifymongodnotrunning
+from fabfile import (setuprepo, installpackage, start_mongod, stop_mongod,
+                     verifymongodrunning, verifymongodnotrunning)
 import os
 import time
 
@@ -37,11 +38,7 @@ ami_id = 'ami-3f854656'
 label = 'centos 56 64'
 ami_id = 'ami-7385461a'
 
-
-
-
-
-#http://aws.amazon.com/solutions/global-solution-providers/redhat/
+# http://aws.amazon.com/solutions/global-solution-providers/redhat/
 
 # rhel 6.4
 #
@@ -71,22 +68,17 @@ label = 'rhel 62'
 ami_id = 'ami-876c05ee'
 env['user'] = 'root'
 
-
-
-
 # debian squeeze
 #
-#label = 'debian squeeze'
-#ami_id = 'ami-8568efec'
-#env['user'] = 'admin'
+# label = 'debian squeeze'
+# ami_id = 'ami-8568efec'
+# env['user'] = 'admin'
 
 # ubuntu 12.04
 #
 label = 'ubuntu 12.04'
 ami_id = 'ami-3fec7956'
 env['user'] = 'ec2-user'
-
-
 
 region = 'us-east-1'
 key_name = 'admin1'
@@ -107,7 +99,8 @@ enabled=1"""
 env.repo_file = "/etc/yum.repos.d/10gen.repo"
 env.post_repo_cmd = "yum --quiet makecache"
 env.packages = "mongo-10gen-unstable mongo-10gen-unstable-server"
-# full commands to install/uninstall env.packages with string replaced by env.packages setting above
+# full commands to install/uninstall env.packages with string replaced by
+# env.packages setting above
 #
 env.install_cmd = "yum install --assumeyes --quiet %s"
 env.uninstall_cmd = "yum --assumeyes --quiet remove %s"
@@ -119,41 +112,39 @@ env.uninstall_cmd = "yum --assumeyes --quiet remove %s"
 env['user'] = 'admin'
 env.init_script_name = "mongodb"
 env.init_service_name = "mongodb"
-env.pre_repo_cmd = "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
-#env.repo_String = "deb http://temp-ubuntu1204-7.10gen.cc/downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen"
-env.repo_String = "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen"
+pre_repo_cmd = "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
+env.pre_repo_cmd = pre_repo_cmd
+repo_String = \
+    "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen"
+env.repo_String = repo_String
 env.repo_file = "/etc/apt/sources.list.d/10gen.list"
 env.post_repo_cmd = "apt-get --yes --quiet update"
 env.packages = "mongodb-10gen-unstable"
-# full commands to install/uninstall env.packages with string replaced by env.packages setting above
+# full commands to install/uninstall env.packages with string replaced by
+# env.packages setting above
 #
 env.install_cmd = "apt-get --yes --quiet install %s"
 env.uninstall_cmd = "apt-get --yes --quiet purge %s"
 
 # end debian config
 
-
-
-
-
-
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 if not AWS_ACCESS_KEY_ID:
-  print "required environment variable missing: AWS_ACCESS_KEY_ID:"
-  exit(2)
+    print "required environment variable missing: AWS_ACCESS_KEY_ID:"
+    exit(2)
 
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 if not AWS_SECRET_ACCESS_KEY:
-  print "required environment variable missing: AWS_SECRET_ACCESS_KEY:"
-  exit(2)
+    print "required environment variable missing: AWS_SECRET_ACCESS_KEY:"
+    exit(2)
 
 
 conn = boto.ec2.connect_to_region(region)
 
 res = conn.run_instances(
   ami_id,
-  key_name = key_name,
-  instance_type = instance_type,
+  key_name=key_name,
+  instance_type=instance_type,
   security_groups=[security_group])
 
 timeout = 60
@@ -174,14 +165,14 @@ print "instance state: %s" % inst.state
 elapsed_seconds = (datetime.datetime.now() - start_time).seconds
 
 while inst.state == 'pending' and elapsed_seconds <= timeout:
-  time.sleep(2)
-  inst.update()
-  print "instance state: %s" % inst.state
-  elapsed_seconds = (datetime.datetime.now() - start_time).seconds
+    time.sleep(2)
+    inst.update()
+    print "instance state: %s" % inst.state
+    elapsed_seconds = (datetime.datetime.now() - start_time).seconds
 
 print "%d seconds elapsed" % elapsed_seconds
 if inst.state != 'running':
-  print "Unknown instance state: %s" % inst.state
+    print "Unknown instance state: %s" % inst.state
 print "connecting..."
 print "dns name: %s " % inst.dns_name
 
