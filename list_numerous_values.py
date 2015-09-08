3c069b43aa01dcb106e84845a3c186365a9d5957
Ernie Hershey <github@ernie.org>
2015-09-07 23:27:11 -0400

    pep8

--- list_numerous_values.py
+++ list_numerous_values.py
@@ -7,24 +7,26 @@ import argparse
 
 parser = argparse.ArgumentParser(description='List values for Numerous metric')
 parser.add_argument('-n', '--number',
-                    help='Number of values to retrieve', type = int, default = 10)
+                    help='Number of values to retrieve', type=int, default=10)
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-m', '--metric-id', help='Metric ID')
-group.add_argument('-l', '--labelsearch', help='Metric label regular expression search')
+group.add_argument('-l', '--labelsearch',
+                   help='Metric label regular expression search')
 args = parser.parse_args()
 
 if args.labelsearch:
-  metric_ids = [ metric['id'] for metric in numerousapp.get_metrics(labelsearch = args.labelsearch) ]
+    metric_ids = [metric['id'] for metric in
+                  numerousapp.get_metrics(labelsearch=args.labelsearch)]
 else:
-  metric_ids = [ args.metric_id ]
+    metric_ids = [args.metric_id]
 
 for metric_id in metric_ids:
-  values = numerousapp.get_metric_values(metric_id = metric_id);
-  metric = numerousapp.get_metric(metric_id = metric_id);
-  print "Metric: " + metric['label']
-  printed = 0
-  for value in values:
-      print "%s: %s" % (value['updated_pretty'], value['value'])
-      printed += 1
-      if printed > args.number:
-        break
+    values = numerousapp.get_metric_values(metric_id=metric_id)
+    metric = numerousapp.get_metric(metric_id=metric_id)
+    print "Metric: " + metric['label']
+    printed = 0
+    for value in values:
+        print "%s: %s" % (value['updated_pretty'], value['value'])
+        printed += 1
+        if printed > args.number:
+            break
