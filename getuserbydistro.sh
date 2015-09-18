92622e1783abe9edc0713d30648913f10d65a257
Ernie Hershey <github@ernie.org>
2015-09-18 00:00:43 -0400

    Detect ubuntu distro

--- getuserbydistro.sh
+++ getuserbydistro.sh
@@ -6,6 +6,12 @@ getuserbydistro ()
       echo Administrator
       return
     fi
+    if echo $distro | grep ubuntu > /dev/null 2>&1
+    then
+      echo ubuntu
+      return
+    fi
+ 
     user=$(
       grep -l ^$distro: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | xargs egrep --no-filename ^$distro:\|user: | grep -A 1 ^$distro: | grep -v ^$distro: | grep -v -- --  | tail -1 | cut -f2 -d: | tr -d " "
    )
