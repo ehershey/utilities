59a31f9738102ee1cebcd42802f46c981c959d58
Ernie Hershey <github@ernie.org>
2015-09-28 11:47:36 -0400

    pep8

--- ernie.py
+++ ernie.py
@@ -7,31 +7,32 @@ import withings_auth
 from withings import WithingsAccount
 
 POUNDS_IN_KILOGRAMS = 2.20462262
+
+
 def get_withings_weight(fromdate, todate):
 
-  # Withings API
-  withings = WithingsAccount(withings_auth.username, withings_auth.password)
-  user = withings.get_user_by_shortname(withings_auth.shortname)
-  if not user:
-      print 'could not find user: %s' % withings_auth.shortname
-      return
-  if not user.ispublic:
-      print 'user %s has not opened withings data' % withings_auth.shortname
-      return
-  startdate = int(time.mktime(fromdate.timetuple()))
-  enddate = int(time.mktime(todate.timetuple())) + 86399
-  groups = user.get_measure_groups(startdate=startdate, enddate=enddate)
+    # Withings API
+    withings = WithingsAccount(withings_auth.username, withings_auth.password)
+    user = withings.get_user_by_shortname(withings_auth.shortname)
+    if not user:
+            print 'could not find user: %s' % withings_auth.shortname
+            return
+    if not user.ispublic:
+            print 'user %s has not opened withings data' % \
+                withings_auth.shortname
+            return
+    startdate = int(time.mktime(fromdate.timetuple()))
+    enddate = int(time.mktime(todate.timetuple())) + 86399
+    groups = user.get_measure_groups(startdate=startdate, enddate=enddate)
 
-  weights = []
-  for group in groups:
-    dt = group.get_datetime()
-    weight = group.get_weight()
-    fat_ratio = group.get_fat_ratio()
-    # print "date: %s " % dt
-    # print "weight: %f lb" % (weight * POUNDS_IN_KILOGRAMS);
-    # print "fat ratio: %f " % (fat_ratio or 0);
-    if weight:
-        weights.append(weight * POUNDS_IN_KILOGRAMS)
-  return weights
-
-
+    weights = []
+    for group in groups:
+        dt = group.get_datetime()
+        weight = group.get_weight()
+        fat_ratio = group.get_fat_ratio()
+        # print "date: %s " % dt
+        # print "weight: %f lb" % (weight * POUNDS_IN_KILOGRAMS);
+        # print "fat ratio: %f " % (fat_ratio or 0);
+        if weight:
+                weights.append(weight * POUNDS_IN_KILOGRAMS)
+    return weights
