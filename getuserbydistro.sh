20beadde40836700e9939509039cf21fdd5e8919
Ernie Hershey <github@ernie.org>
2015-10-17 23:33:10 -0400

    whitespace

--- getuserbydistro.sh
+++ getuserbydistro.sh
@@ -27,8 +27,8 @@ getuserbydistro ()
       echo ec2-user
       return
     fi
- 
- 
+
+
     user=$(
       grep -l ^$distro: ~/git/mci/config_prod/distros.yml* ~/git/mci/config_prod/distros/* ~/distros.yml* | xargs egrep --no-filename ^$distro:\|user: | grep -A 1 ^$distro: | grep -v ^$distro: | grep -v -- --  | tail -1 | cut -f2 -d: | tr -d " "
    )
