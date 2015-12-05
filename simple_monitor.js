478caaae19f1afa35c867613e8f7550d15227848
Ernie Hershey <github@ernie.org>
2015-12-05 12:26:47 -0500

    turn off ernie.org checks while dns is fried

--- simple_monitor.js
+++ simple_monitor.js
@@ -102,27 +102,27 @@ var url_configs = [
     ignore_text: '',
     negated: true
   },
-  {
-    url: 'http://frau.ernie.org/',
-    cell_selector: "body:contains(frau.ernie.org)",
-    text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
-    ignore_text: '',
-    negated: true
-  },
-  {
-    url: 'http://dropbox.ernie.org/weather.html',
-    cell_selector: "body:contains(Weather widget)",
-    text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
-    ignore_text: '',
-    negated: true
-  },
-  {
-    url: 'http://numpebble.ernie.org/config.html',
-    cell_selector: "body:contains(Title)",
-    text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
-    ignore_text: '',
-    negated: true
-  },
+  // {
+    // url: 'http://frau.ernie.org/',
+    // cell_selector: "body:contains(frau.ernie.org)",
+    // text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
+    // ignore_text: '',
+    // negated: true
+  // },
+  // {
+    // url: 'http://dropbox.ernie.org/weather.html',
+    // cell_selector: "body:contains(Weather widget)",
+    // text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
+    // ignore_text: '',
+    // negated: true
+  // },
+  // {
+    // url: 'http://numpebble.ernie.org/config.html',
+    // cell_selector: "body:contains(Title)",
+    // text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
+    // ignore_text: '',
+    // negated: true
+  // },
   // {
     // url: 'http://cov1.bci.10gen.cc:8080/',
     // cell_selector: "body:contains(Coverity)",
@@ -130,20 +130,20 @@ var url_configs = [
     // ignore_text: '',
     // negated: true
   // },
-  {
-    url: 'http://goeverywhere.ernie.org/get_stats.cgi',
-    cell_selector: "body:contains(oldest_point_timestamp)",
-    text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
-    ignore_text: '',
-    negated: true
-  },
-  {
-    url: 'http://goeverywhere.ernie.org/get_points.cgi?from=09/15/2014&to=09/15/2014&min_lon=-80&max_lon=80&min_lat=-90&max_lat=90&bound_string=%28%2840.661127887535734%2C%20-74.28702794525663%29%2C%20%2840.77718145714685%2C%20-73.68380986664334%29%29&rind=1/1',
-    cell_selector: "body:contains(point)",
-    text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
-    ignore_text: '',
-    negated: true
-  },
+  // {
+    // url: 'http://goeverywhere.ernie.org/get_stats.cgi',
+    // cell_selector: "body:contains(oldest_point_timestamp)",
+    // text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
+    // ignore_text: '',
+    // negated: true
+  // },
+  // {
+    // url: 'http://goeverywhere.ernie.org/get_points.cgi?from=09/15/2014&to=09/15/2014&min_lon=-80&max_lon=80&min_lat=-90&max_lat=90&bound_string=%28%2840.661127887535734%2C%20-74.28702794525663%29%2C%20%2840.77718145714685%2C%20-73.68380986664334%29%29&rind=1/1',
+    // cell_selector: "body:contains(point)",
+    // text_finder_from_cell_jqobj: function(jqobj) { return jqobj.text(); },
+    // ignore_text: '',
+    // negated: true
+  // },
 
   {
     url: 'https://github.com/ehershey',
