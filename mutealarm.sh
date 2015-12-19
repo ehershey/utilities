831cdeaac98ec7000d94a46b5806e9fc48cc51a3
Ernie Hershey <github@ernie.org>
2015-12-19 01:20:31 -0500

    loop while killing afplay

--- mutealarm.sh
+++ mutealarm.sh
@@ -2,7 +2,11 @@
 #
 # Kill alarm processes started by alarm.sh
 #
+
 osxmute
-killall afplay
-sleep 1
-killall afplay
+while ps auwx | grep -v grep | grep afplay
+do
+  sudo killall afplay
+  sleep 1
+  sudo killall afplay
+done
