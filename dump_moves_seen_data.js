53917afbc55b2768308c830e4d13a0f994025edb
Ernie Hershey <github@ernie.org>
2016-01-20 23:40:22 -0500

    semicolon

--- dump_moves_seen_data.js
+++ dump_moves_seen_data.js
@@ -15,7 +15,7 @@ db.getCollection(collection_name).find().toArray().forEach(function(activity) {
   var first_seen = activity.application_metadata.first_seen;
   var start_time = ISODate(activity.startTime.replace(/(\d\d\d\d)(\d\d)(\d\d)/, "$1-$2-$3"));
 
-  var diff_millis = ( first_seen.getTime() - start_time.getTime() )
+  var diff_millis = ( first_seen.getTime() - start_time.getTime() );
 
   var diff_seconds = diff_millis / 1000;
 
