9013d1403955bf57a3adadbae3786f88ace80e41
Ernie Hershey <github@ernie.org>
2016-03-21 09:11:31 -0400

    meal rearranging and comments

--- foodlogger/meals.py
+++ foodlogger/meals.py
@@ -4,15 +4,26 @@
 import datetime
 
 
+# Choose a meal based on time -
+# 3am - 10am - Breakfast
+# 10am - noon - Snack 1
+# noon - 2pm - Lunch
+# 2pm - 5pm - Snack 2
+# 5pm - 8pm - Dinner
+# 8pm - 3am - Snack 3
+
 def get_default_meal(timestamp=datetime.datetime.now()):
-    # Before noon is breakfast
-    #
-    if timestamp.hour < 12:
-        return "breakfast"
-    # Before 5pm is lunch
-    #
+    if timestamp.hour < 3:
+        return "Snack 3"
+    elif timestamp.hour < 10:
+        return "Breakfast"
+    elif timestamp.hour < 12:
+        return "Snack 1"
+    elif timestamp.hour < 14:
+        return "Lunch"
     elif timestamp.hour < 17:
-        return "lunch"
-    # 5 and later is dinner
+        return "Snack 2"
+    elif timestamp.hour < 20:
+        return "Dinner"
     else:
-        return "dinner"
+        return "Snack 3"
