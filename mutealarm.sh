fa25b3875625f30174149acca68e9c3062640b66
Ernie Hershey <github@ernie.org>
2016-04-03 09:58:25 -0400

    sudoers line

--- mutealarm.sh
+++ mutealarm.sh
@@ -2,6 +2,9 @@
 #
 # Kill alarm processes started by alarm.sh
 #
+# Works best with sudoers addition:
+#
+# %admin ALL=NOPASSWD:/usr/bin/killall afplay # for alarmkill.sh
 
 osxmute
 while ps auwx | grep -v grep | grep afplay
