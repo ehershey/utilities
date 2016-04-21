b434ff575445498ceb832774ae6e3e1f2968d5fd
Ernie Hershey <github@ernie.org>
2016-04-21 11:02:48 -0400

    Bringing up to date

--- mongodb_autobuild.sh
+++ mongodb_autobuild.sh
@@ -3,10 +3,24 @@
 # Automatically build MongoDB every night
 #
 #
+set -o verbose
+set -o pipefail
+set -o errexit
+set -o nounset
+
+# Needed at least for sysctl
+#
+export PATH=$PATH:/usr/sbin
 
 BUILD_DIR=/Users/ernie/git/mongo.autobuild
 REV=master
-SCONS_ARGS="-j8 --ssl all"
+#SCONS_ARGS="--use-new-tools all dist dist-debugsymbols distsrc-tgz --ssl --allocator=system -j$(sysctl -n hw.logicalcpu) --osx-version-min=10.7 --libc++ LINKFLAGS=-L/usr/local/opt/openssl/lib CCFLAGS=-I/usr/local/opt/openssl/include"
+SCONS_ARGS="all --ssl --allocator=system -j$(sysctl -n hw.logicalcpu) --osx-version-min=10.7 --libc++ LINKFLAGS=-L/usr/local/opt/openssl/lib CCFLAGS=-I/usr/local/opt/openssl/include"
+exe=
+gorootvars=
+gitvars=
+tooltags="-tags 'ssl sasl'"
+OS=osx
 
 
 export PATH=$PATH:/usr/local/bin
@@ -25,6 +39,41 @@ done
 cd "$BUILD_DIR"
 git fetch --all
 git reset --hard "origin/$REV"
+
+# build tools
+#
+
+# create the target directory for the binaries
+mkdir -p src/mongo-tools
+
+# clone into a different path so the binaries and package directory
+# names do not conflict
+cd src/mongo-tools-repo
+git fetch --all
+git reset --hard "origin/$REV"
+
+# make sure newlines in the scripts are handled correctly by windows
+if [ "Windows_NT" = "$OS" ]; then
+    set -o igncr
+fi;
+
+git checkout $REV
+. ./set_gopath.sh
+
+# In RHEL 5.5, /usr/bin/ld can't handle --build-id parameters, so
+# use a wrapper if it's present on the system
+#
+if [ -d /opt/ldwrapper/bin ]
+then
+  export PATH=/opt/ldwrapper/bin:$PATH
+fi
+
+for i in bsondump mongostat mongofiles mongoexport mongoimport mongorestore mongodump mongotop mongooplog; do
+     #eval ${gorootvars} go build -gccgoflags -I/usr/local/opt/openssl/include -ldflags \"-L/usr/local/opt/openssl/lib -X github.com/mongodb/mongo-tools/common/options.Gitspec $(git rev-parse HEAD) -X github.com/mongodb/mongo-tools/common/options.VersionStr $(git --git-dir ../../.git describe)\" ${tooltags} -o "../mongo-tools/$i${exe}" $i/main/$i.go
+     echo $i;
+done
+
+cd "$BUILD_DIR"
 find . -name \*.pyc -exec rm {} \;
 echo scons $SCONS_ARGS >> autobuild-scons-cmd.txt
 eval scons $SCONS_ARGS
