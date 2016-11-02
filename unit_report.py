cfb9c2332b09ab11d8c8277682cb7fb974606f91
Ernie Hershey <github@ernie.org>
2016-11-02 19:52:16 -0400

    remove numerous calls and add 2016 info

--- unit_report.py
+++ unit_report.py
@@ -8,12 +8,9 @@ import re
 import sys
 import time
 from os.path import expanduser
-from numerousapp import update_metric_value, get_metric_value
 
 
 parser = argparse.ArgumentParser(description='Generate calorie report')
-parser.add_argument('-s', '--skip-numerous',
-                    action='store_true', help="Don't post to Numerous")
 args = parser.parse_args()
 
 RUNNING_CALORIE_MULTIPLIER = 118
@@ -60,17 +57,10 @@ input_2014 = 0
 input_today_2014_diff = 0
 input_2014_2013_diff = 0
 input_2013_2014_diff = 0
-surplus_today = 0
-surplus_yesterday = 0
-surplus_2days = 0
-surplus_2014 = 0
-surplus_today_2014_diff = 0
-surplus_yesterday_2014_diff = 0
-surplus_2days_2014_diff = 0
-surplus_2016_total = 0
 input_2016 = 0
 input_2016_total = 0
-surplus_2016_2014_diff = 0
+input_2016_2013_diff = 0
+input_2016_2014_diff = 0
 
 if today_summary and today_summary['Calories']:
     input_today = round(today_summary['calories_numeric'], 2)
@@ -102,7 +92,8 @@ input_today_2014_diff = round(input_2014 - input_today, 2)
 input_today_2013_diff = round(input_2013 - input_today, 2)
 input_yesterday_2013_diff = round(input_2013 - input_yesterday, 2)
 input_yesterday_2014_diff = round(input_2014 - input_yesterday, 2)
-input_2014_2013_diff = round(input_2013 - input_2014, 2)
+input_2016_2013_diff = round(input_2013 - input_2014, 2)
+input_2016_2014_diff = round(input_2014 - input_2016, 2)
 input_2013_2014_diff = round(input_2014 - input_2013, 2)
 
 
@@ -150,23 +141,6 @@ units_average_2days = os.popen(
 
 sys.stderr.write("done with cuts\n")
 
-surplus_today = float(input_today) - \
-    (float(units_today) + resting_daily_calories)
-surplus_yesterday = float(input_yesterday) - \
-    (float(units_yesterday) + resting_daily_calories)
-surplus_2days = (surplus_today + surplus_yesterday) / 2
-surplus_2014 = float(input_2014) - \
-    (float(units_average_2014) + resting_daily_calories)
-surplus_2016 = float(input_2016) - \
-    (float(units_average_2016) + resting_daily_calories)
-surplus_2016_total = float(input_2016_total) - (float(units_2016_total) +
-                                                resting_daily_calories * int(day_count_2016))
-surplus_2016_2014_diff = round(surplus_2016 - surplus_2014, 2)
-
-surplus_today_2014_diff = round(surplus_2014 - surplus_today, 2)
-surplus_yesterday_2014_diff = round(surplus_2014 - surplus_yesterday, 2)
-surplus_2days_2014_diff = round(surplus_2014 - surplus_2days, 2)
-
 units_today_2013_diff = float(units_today) - float(units_average_2013)
 units_today_2014_diff = float(units_today) - float(units_average_2014)
 units_yesterday_2013_diff = float(units_yesterday) - float(units_average_2013)
@@ -183,6 +157,10 @@ units_average_2days_2014_diff = float(
     units_average_2days) - float(units_average_2014)
 units_average_2014_2013_diff = float(
     units_average_2014) - float(units_average_2013)
+units_average_2016_2014_diff = float(
+    units_average_2016) - float(units_average_2014)
+units_average_2016_2013_diff = float(
+    units_average_2016) - float(units_average_2013)
 units_average_2013_2014_diff = float(
     units_average_2013) - float(units_average_2014)
 
@@ -193,21 +171,6 @@ if units_today_2013_diff > 0:
     placeholder['today_class'] = "positive_diff"
 else:
     placeholder['today_class'] = "negative_diff"
-
-if surplus_today < surplus_2014:
-    placeholder['surplus_class'] = "positive_diff"
-else:
-    placeholder['surplus_class'] = "negative_diff"
-
-if surplus_2days < surplus_2014:
-    placeholder['surplus_2days_class'] = "positive_diff"
-else:
-    placeholder['surplus_2days_class'] = "negative_diff"
-
-if surplus_2016_2014_diff > 0:
-    placeholder['surplus_2016_class'] = "negative_diff"
-else:
-    placeholder['surplus_2016_class'] = "positive_diff"
 
 if input_today_2014_diff > 0:
     placeholder['input_class'] = "positive_diff"
@@ -263,24 +226,9 @@ placeholder['input_2014_2013_diff'] = input_2014_2013_diff
 placeholder['input_2013_2014_diff'] = input_2013_2014_diff
 placeholder['input_today_url'] = input_today_url
 placeholder['input_yesterday_url'] = input_yesterday_url
-placeholder['surplus_today'] = surplus_today
-placeholder['surplus_2014'] = surplus_2014
-placeholder['surplus_2016'] = surplus_2016
-placeholder['surplus_yesterday'] = surplus_yesterday
-placeholder['surplus_2days'] = surplus_2days
-placeholder['surplus_today_2014_diff'] = surplus_today_2014_diff
-placeholder['surplus_yesterday_2014_diff'] = surplus_yesterday_2014_diff
-placeholder['surplus_2days_2014_diff'] = surplus_2days_2014_diff
-placeholder['surplus_2016_2014_diff'] = surplus_2016_2014_diff
-placeholder['surplus_2016_total'] = surplus_2016_total
-
-
-# echo "units_today: $units_today"
-# echo "units_average: $units_average"
-# echo "units_average_2013: $units_average_2013"
-# echo "units_average_2014: $units_average_2014"
-# echo "units_average_7days: $units_average_7days"
-
+placeholder['units_average_2016'] = units_average_2016
+placeholder['units_average_2016_2013_diff'] = units_average_2016_2013_diff
+placeholder['units_average_2016_2014_diff'] = units_average_2016_2014_diff
 
 if __name__ == '__main__':
 
@@ -289,52 +237,6 @@ if __name__ == '__main__':
 
     formated = template.format(**placeholder)
     print formated
-
-    # Report to numerous
-    #
-    # TODO: move this out of report script
-    #
-
-    # Only post if we have data for today or it's been long enough that we should have data for today
-    #
-    if not args.skip_numerous and (units_today > 0 or datetime.datetime.now().hour > MIN_HOUR_FOR_ZERO_POST):
-        running_goal = get_metric_value(5818738672989877729)
-        walking_goal = get_metric_value(7758304728227275179)
-        biking_goal = get_metric_value(5595761423440323786)
-        if ran_today == "" or not ran_today:
-            ran_today = 0
-        if biked_today == "" or not biked_today:
-            biked_today = 0
-        if walked_today == "" or not walked_today:
-            walked_today = 0
-        if running_goal:
-            running_goal = running_goal['value']
-        else:
-            running_goal = 0
-        if walking_goal:
-            walking_goal = walking_goal['value']
-        else:
-            walking_goal = 0
-        if biking_goal:
-            biking_goal = biking_goal['value']
-        else:
-            biking_goal = 0
-
-        biked_today = float(biked_today)
-        ran_today = float(ran_today)
-        walked_today = float(walked_today)
-        update_metric_value(8748753640388107935, running_goal - ran_today)
-        update_metric_value(94526752002396912, walking_goal - walked_today)
-        update_metric_value(111285933456617558, biking_goal - biked_today)
-        # update_metric_value(6359390767342201980, units_yesterday, updated = "2014-06-15T23:59:59.000Z")
-        update_metric_value(6359390767342201980, units_today)
-        update_metric_value(7670190745339240677, biked_today)
-        update_metric_value(3043034116976301897, walked_today)
-        update_metric_value(3398081990673437243, ran_today)
-        update_metric_value(5212351794073589044, units_today_2013_diff)
-        update_metric_value(7170780739467042866, units_average_2days)
-        update_metric_value(1242812163656294116, units_average_2days_2013_diff)
-        update_metric_value(3766962275739755372, minutes_since_moves_update)
 
 
 def get_daily_calorie_goal():
