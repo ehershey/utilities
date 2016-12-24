58068173faf879ba934539ae6bb51c379cd7ad7f
Ernie Hershey <github@ernie.org>
2016-12-24 10:00:53 -0500

    Add 30 day things, new column

--- unit_report.py
+++ unit_report.py
@@ -30,6 +30,20 @@ yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
 current_year = today.year
 previous_year = current_year - 1
 
+current_month = today.month
+previous_month = current_month - 1
+
+current_day = today.day
+previous_day = current_day - 1
+
+# zero padding
+#
+# current_month = "%02d" % (current_month)
+# previous_month = "%02d" % (previous_month)
+
+# current_day = "%02d" % (current_day)
+# previous_day = "%02d" % (previous_day)
+
 today_summary = db.nutrition_summary.find_one(
     {"date": today.strftime("%B %-d, %Y")})
 yesterday_summary = db.nutrition_summary.find_one(
@@ -70,6 +84,9 @@ units_average = os.popen(
     "cut -f10 -d, %s| awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
 units_yesterday = os.popen(
     "cut -f10 -d, %s  | head -3 | tail -1" % MOVES_CSV_FILENAME).read().rstrip()
+
+units_today_last_year = os.popen(
+    "grep ^%d-%d-%d %s | cut -f10 -d, " % (previous_year, current_month, current_day, MOVES_CSV_FILENAME)).read().rstrip()
 sys.stderr.write("in cuts 1\n")
 units_average_previous_year = os.popen(
     "grep ^%d- %s | cut -f10 -d, | awk '{ total += $1; count++ } END { print total/count }'" % (previous_year, MOVES_CSV_FILENAME)).read().rstrip()
@@ -80,6 +97,8 @@ units_current_year_total = os.popen(
 sys.stderr.write("in cuts 2\n")
 day_count_current_year = os.popen(
     "grep ^%d- %s | cut -f10 -d, | awk '{ total += $1; count++ } END { print count }'" % (current_year, MOVES_CSV_FILENAME)).read().rstrip()
+units_average_30days = os.popen(
+    "head -8 %s | tail -30 | cut -f10 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
 units_average_7days = os.popen(
     "head -8 %s | tail -7 | cut -f10 -d, | awk '{ total += $1; count++ } END { print total/count }'" % MOVES_CSV_FILENAME).read().rstrip()
 units_average_2days = os.popen(
@@ -92,30 +111,61 @@ units_yesterday_previous_year_diff = float(units_yesterday) - float(units_averag
 units_average_previous_year_diff = float(units_average) - float(units_average_previous_year)
 units_average_7days_previous_year_diff = float(
     units_average_7days) - float(units_average_previous_year)
+units_average_30days_previous_year_diff = float(
+    units_average_30days) - float(units_average_previous_year)
 units_average_2days_previous_year_diff = float(
     units_average_2days) - float(units_average_previous_year)
 units_average_current_year_previous_year_diff = float(
     units_average_current_year) - float(units_average_previous_year)
+units_today_last_year_previous_year_diff = float(units_today_last_year) - float(units_average_previous_year)
+
+units_today_30days_diff = float(units_today) - float(units_average_30days)
+units_yesterday_30days_diff = float(units_yesterday) - float(units_average_30days)
+units_average_30days_diff = float(units_average) - float(units_average_30days)
+units_average_7days_30days_diff = float(
+    units_average_7days) - float(units_average_30days)
+units_average_2days_30days_diff = float(
+    units_average_2days) - float(units_average_30days)
+units_average_current_year_30days_diff = float(
+    units_average_current_year) - float(units_average_30days)
+units_average_previous_year_30days_diff = float(
+    units_average_previous_year) - float(units_average_30days)
+units_today_last_year_30days_diff = float(units_today_last_year) - float(units_average_30days)
 
 minutes_since_moves_update = (datetime.datetime.now(
 ) - datetime.datetime.fromtimestamp(os.path.getmtime(MOVES_CSV_FILENAME))).seconds / 60
 
-placeholder['units_today_previous_year_diff'] = units_today_previous_year_diff
 placeholder['units_today'] = units_today
-placeholder['units_yesterday_previous_year_diff'] = units_yesterday_previous_year_diff
+placeholder['units_today_last_year'] = units_today_last_year
 placeholder['units_yesterday'] = units_yesterday
 placeholder['units_average'] = units_average
-placeholder['units_average_previous_year_diff'] = units_average_previous_year_diff
+placeholder['units_average_30days'] = units_average_30days
 placeholder['units_average_previous_year'] = units_average_previous_year
 placeholder['units_average_7days'] = units_average_7days
-placeholder['units_average_7days_previous_year_diff'] = units_average_7days_previous_year_diff
 placeholder['units_average_2days'] = units_average_2days
-placeholder['units_average_2days_previous_year_diff'] = units_average_2days_previous_year_diff
 placeholder['now'] = time.ctime()
 placeholder['moves_csv_modified'] = time.ctime(
     os.path.getmtime(MOVES_CSV_FILENAME))
 placeholder['units_average_current_year'] = units_average_current_year
+
+placeholder['units_today_previous_year_diff'] = units_today_previous_year_diff
+placeholder['units_today_last_year_previous_year_diff'] = units_today_last_year_previous_year_diff
+placeholder['units_yesterday_previous_year_diff'] = units_yesterday_previous_year_diff
+placeholder['units_average_previous_year_diff'] = units_average_previous_year_diff
+placeholder['units_average_7days_previous_year_diff'] = units_average_7days_previous_year_diff
+placeholder['units_average_30days_previous_year_diff'] = units_average_30days_previous_year_diff
+placeholder['units_average_2days_previous_year_diff'] = units_average_2days_previous_year_diff
 placeholder['units_average_current_year_previous_year_diff'] = units_average_current_year_previous_year_diff
+
+placeholder['units_today_30days_diff'] = units_today_30days_diff
+placeholder['units_today_last_year_30days_diff'] = units_today_last_year_30days_diff
+placeholder['units_yesterday_30days_diff'] = units_yesterday_30days_diff
+placeholder['units_average_30days_diff'] = units_average_30days_diff
+placeholder['units_average_7days_30days_diff'] = units_average_7days_30days_diff
+placeholder['units_average_2days_30days_diff'] = units_average_2days_30days_diff
+placeholder['units_average_current_year_30days_diff'] = units_average_current_year_30days_diff
+placeholder['units_average_previous_year_30days_diff'] = units_average_previous_year_30days_diff
+
 placeholder['current_year'] = current_year
 placeholder['previous_year'] = previous_year
 placeholder['today_class'] = ""
