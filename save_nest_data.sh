31e8f86c97869488a8c0bdacb20d7acdddb6243e
Ernie Hershey <github@ernie.org>
2017-02-22 23:45:19 -0500

    save Nest data

---
+++ save_nest_data.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+#
+# Retrieve Nest Thermostat state from the Nest API and store in MongoDB
+#
+#
+auth_file="$(dirname "$0")"/nest_auth
+
+# Should contain
+# NEST_ACCESS_TOKEN=xxx
+# NEST_DEVICE_ID=xxx
+
+if [ -e "$auth_file" ]
+then
+  . "$auth_file"
+else
+  echo "Can't find auth file: $auth_file"
+  exit 1
+fi
+curl -L https://developer-api.nest.com/devices/thermostats/$NEST_DEVICE_ID.json\?auth\=$NEST_ACCESS_TOKEN | mongoimport --db nest --collection thermostat_log
