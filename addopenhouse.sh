c9f991fca2fa213e31d2fd827a35a763f4c24e46
Ernie Hershey <github@ernie.org>
2017-03-01 14:28:07 -0500

    add --no-dupe-check parm and fix gcalcli call

--- addopenhouse.sh
+++ addopenhouse.sh
@@ -3,9 +3,18 @@
 # Add open house to calendar
 #
 # Usage:
-# addopenhouse.sh <title> <date> <description> <duration - minutes> <location>
+# addopenhouse.sh [ --no-dupe-check ] <title> <date> <description> <duration - minutes> <location>
 #
 CALENDAR="Personal"
+
+if [ "$1" = "--no-dupe-check" ]
+then
+  no_dupe_check="true"
+  shift
+else
+  no_dupe_check=""
+fi
+
 
 TITLE="$1"
 DATE="$2"
@@ -21,8 +30,8 @@ fi
 
 REMINDER=1440 # day
 
-output="$(gcalcli --cache --cal "$CALENDAR" search "\"$TITLE\"" --details url | grep .)"
-if [ ! "$(echo "$output" | grep 'No Events Found...')" ]
+output="$(gcalcli --cache --calendar "$CALENDAR" search "\"$TITLE\"" --details url | grep .)"
+if [ ! "$no_dupe_check" -a ! "$(echo "$output" | grep 'No Events Found...')" ]
 then
   echo "Probable duplicate:"
   echo "$output"
@@ -33,8 +42,8 @@ NOAPTTITLE="$(echo "$TITLE" | sed 's/ APT.*//')"
 NOAPTTITLE="$(echo "$NOAPTTITLE" | sed 's/ apt.*//')"
 NOAPTTITLE="$(echo "$NOAPTTITLE" | sed 's/ Apt.*//')"
 
-output="$(gcalcli --cache --cal "$CALENDAR" search "\"$NOAPTTITLE\"" --details url | grep .)"
-if [ ! "$(echo "$output" | grep 'No Events Found...')" ]
+output="$(gcalcli --cache --calendar "$CALENDAR" search "\"$NOAPTTITLE\"" --details url | grep .)"
+if [ ! "$no_dupe_check" -a ! "$(echo "$output" | grep 'No Events Found...')" ]
 then
   echo "Probable duplicate:"
   echo "$output"
@@ -42,7 +51,7 @@ then
 fi
 
 set -x
-output="$(gcalcli add --cal "$CALENDAR" --title "$TITLE" --when "$DATE" --duration "$DURATION" --description "$DESCRIPTION" --where "$LOCATION" --reminder "$REMINDER" --details url)"
+output="$(gcalcli add --calendar "$CALENDAR" --title "$TITLE" --when "$DATE" --duration "$DURATION" --description "$DESCRIPTION" --where "$LOCATION" --reminder "$REMINDER" --details url)"
 
 url="$(echo "$output" | tr \ \\t \\n | grep http | head -1)"
 if [ ! "$url" ]
