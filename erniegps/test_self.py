01b242c2bef315bc277956b599b0c0ad673df057
Ernie Hershey <github@ernie.org>
2017-06-02 08:51:41 -0400

    New tests, fix paths

--- erniegps/test_self.py
+++ erniegps/test_self.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Test erniegps module
 """
@@ -6,13 +7,8 @@ Test erniegps module
 
 import erniegps
 import m26
+import os
 import pytest
-
-#def test_error_on_too_few_trackpoints():
-  #with pytest.raises(erniegps.ActivityException):
-    #erniegps.get_speed_from_trackpoints([])
-  #with pytest.raises(erniegps.ActivityException):
-    #erniegps.get_speed_from_trackpoints([{}])
 
 
 def test_zero_speed():
@@ -54,14 +50,14 @@ def test_zero_distance():
 
 
 def test_double_read_single_activity():
-    activity = erniegps.read_activity('/home/ernie/Dropbox/Apps/tapiriik/2014-04-29_Walking (1).tcx')
-    activity = erniegps.read_activity('/home/ernie/Dropbox/Apps/tapiriik/2014-04-29_Walking (1).tcx')
+    activity = erniegps.read_activity('{0}/Dropbox/Apps/tapiriik/2014-04-29_Walking (1).tcx'.format(os.environ['HOME']))
+    activity = erniegps.read_activity('{0}/Dropbox/Apps/tapiriik/2014-04-29_Walking (1).tcx'.format(os.environ['HOME']))
     trackpoints = activity['trackpoints']
     assert len(trackpoints) == 8
 
 
 def test_trackpoint_split():
-    activity = erniegps.read_activity('/home/ernie/Dropbox/Apps/tapiriik/2014-04-29_Walking (1).tcx')
+    activity = erniegps.read_activity('{0}/Dropbox/Apps/tapiriik/2014-04-29_Walking (1).tcx'.format(os.environ['HOME']))
     trackpoints = activity['trackpoints']
     (first_half_trackpoints, last_half_trackpoints) = erniegps.split_trackpoints(trackpoints)
     assert len(first_half_trackpoints) == 2
@@ -69,7 +65,35 @@ def test_trackpoint_split():
 
 
 def test_no_negative_split():
-    activity = erniegps.read_activity('/home/ernie/Dropbox/Apps/tapiriik/2014-04-29_Walking (1).tcx')
+    activity = erniegps.read_activity('{0}/Dropbox/Apps/tapiriik/2014-04-29_Walking (1).tcx'.format(os.environ['HOME']))
     activity = erniegps.process_activity(activity)
     assert activity.get('is_negative_split') is False
     assert activity.get('negative_split_depth') == 0
+
+
+def test_empty_distance():
+    """ This track has a trackpoint with no distance that will break an unpatch ggps module """
+    activity = erniegps.read_activity('{0}/Dropbox/Apps/tapiriik/2011-02-03_Running.tcx'.format(os.environ['HOME']))
+    trackpoints = activity['trackpoints']
+
+
+def test_get_activity_type():
+    activity = erniegps.read_activity('{0}/Dropbox/Apps/tapiriik/2017-05-27_New York Running_Running.tcx'.format(os.environ['HOME']))
+    assert activity['activity_type'] == "Running"
+    assert activity['notes'] == "New York Running"
+
+
+def test_has_verbose_starttime():
+    activity = erniegps.read_activity('{0}/Dropbox/Apps/tapiriik/2017-05-27_New York Running_Running.tcx'.format(os.environ['HOME']))
+    assert type(activity['verbose_starttime']) == str
+    assert activity['verbose_starttime'] == '19:03'
+
+
+def test_has_verbose_distance():
+    activity = erniegps.read_activity('{0}/Dropbox/Apps/tapiriik/2017-05-27_New York Running_Running.tcx'.format(os.environ['HOME']))
+    assert type(activity['verbose_distance']) == str
+    assert activity['verbose_distance'] == '10.13 miles'
+
+
+def test_handle_unicode_notes_char():
+    activity = erniegps.read_activity(u'{0}/Dropbox/Apps/tapiriik/2016-06-01_Słomczyn Running_Running.tcx'.format(os.environ['HOME']))
