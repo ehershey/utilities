a5b012db6d693c03a473d6e1236a789177159151
Ernie Hershey <github@ernie.org>
2017-11-20 09:07:13 -0500

    Abort if no difference; display diff and request confirmation before loading

--- load_crontab.sh
+++ load_crontab.sh
@@ -12,6 +12,9 @@ else
   command="crontab"
 fi
 
+tempfile1=$(mktemp)
+tempfile2=$(mktemp)
+
 if [ "$CRON_HOSTNAME" != "$HOSTNAME" ]
 then
   echo "Incorrect hostname: (looks like: $HOSTNAME, should be: $CRON_HOSTNAME)"
@@ -23,4 +26,24 @@ then
   echo "Can't find crontab.env or crontab.txt in $FILEHOME"
   exit 2
 fi
-cat "$FILEHOME/crontab.env" "$FILEHOME/crontab.txt" | $command
+
+$command -l > $tempfile1
+(cat "$FILEHOME/crontab.env" "$FILEHOME/crontab.txt" ) > $tempfile2
+if [[ ! "$(diff $tempfile1 $tempfile2)" ]]
+then
+  echo "No updates. Aborting."
+  exit 1
+fi
+diff $tempfile1 $tempfile2
+rm $tempfile1 $tempfile2
+echo -n "Diff ok? (y/n): "
+read ans
+if [[ "$ans" == "y" ]]
+then
+  echo "Loading crontab."
+  cat "$FILEHOME/crontab.env" "$FILEHOME/crontab.txt" | $command
+else
+  echo Aborting.
+fi
+
+
