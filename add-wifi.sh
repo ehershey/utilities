e60f85b37ea82a0a06f570492268128a43be6acc
Ernie Hershey <github@ernie.org>
2018-01-21 14:44:43 -0500

    Try second interface, pull password from keychain, cleanup

--- add-wifi.sh
+++ add-wifi.sh
@@ -4,21 +4,28 @@ set -o pipefail
 set -o errexit
 set -o nounset
 
-INTERFACE=en0
+INTERFACES="en0 en1"
 WIFIDOC=~/Dropbox//PlainText/wifi.txt
-network=$(networksetup -getairportnetwork $INTERFACE | cut -f2- -d: | sed 's/^ *//')
+
+for INTERFACE in `echo $INTERFACES`
+do
+  if networksetup -getairportnetwork $INTERFACE
+  then
+        network=$(networksetup -getairportnetwork $INTERFACE | cut -f2- -d: | sed 's/^ *//')
+  fi
+done
 echo "Adding to $WIFIDOC"
 if grep -q "$network" "$WIFIDOC"
 then
   echo "WARNING: Appears to be duplicate"
 fi
 echo "Network appears to be: $network"
-echo -n "Enter password: "
-read password
+password="$(security find-generic-password -wa $network)"
+echo "Password appears to be: $password"
 echo -n "Enter location name: "
 read location
 entry="$location - $network / $password"
-echo "Press enter to add entry:"
+echo "Press enter to add entry, ^C to quit:"
 echo "$entry"
 read null
 echo >> $WIFIDOC
