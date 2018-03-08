4d4c087a999c6543a280f6493a608f7139383341
Ernie Hershey <github@ernie.org>
2018-03-08 17:52:25 -0500

    Update Yosemite Half Marathon title

--- racetocal.sh
+++ racetocal.sh
@@ -307,7 +307,8 @@ url_to_test="https://www.virginmoneylondonmarathon.com/en-gb/"
 test_url "$url_to_test" "$expected_title" "$expected_date"
 
 
-expected_title="Yosemite Half Marathon (Direct)"
+#expected_title="Yosemite Half Marathon (Direct)"
+expected_title="Yosemite Half Marathon"
 expected_date="may 12th, 2018"
 url_to_test="http://www.yosemitehalfmarathon.com/"
 
