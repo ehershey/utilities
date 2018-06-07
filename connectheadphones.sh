b4d4f32f9bee46395ff6d13ef61fbf527fa6ba73
Ernie Hershey <github@ernie.org>
2018-06-07 19:11:18 -0400

    block until change seen

--- connectheadphones.sh
+++ connectheadphones.sh
@@ -13,6 +13,17 @@
 DEFAULT_DEVICE_NAME="ODT Privates"
 DEVICE_NAME=${BLUETOOTH_AUDIO_DEVICE_NAME:-$DEFAULT_DEVICE_NAME}
 
+# how long to wait for action to be visible after we believe it to have
+# happened
+#
+TIMEOUT_SECS=10
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+export PATH="$(dirname "$0"):$PATH"
+
 # default is to connect
 action="Connect"
 preposition="to"
@@ -21,6 +32,7 @@ do
   if [ "$1" = "-h" ]
   then
     echo "Usage: $0 [ -h ] [ -d (disconnect) ] [ <device name (default: $DEFAULT_DEVICE_NAME)> ]"
+    exit 2
   elif [ "$1" = "-d" ]
   then
     action="Disconnect"
@@ -35,6 +47,35 @@ echo "${action}ing $preposition $DEVICE_NAME"
 if which blueutil > /dev/null 2>&1
 then
   blueutil --power=1
+fi
+
+if ! which -s lsbt
+then
+  echo "Can't find lsbt executable"
+  exit 4
+fi
+
+if ! lsbt > /dev/null 2>&1 < /dev/null
+then
+  echo "Can't run lsbt executable (no xmlstarlet?)"
+  exit 9
+fi
+
+if [ "$action" = "Disconnect" ]
+then
+  # error if trying to disconnect and it's not connected
+  if [ ! "$(lsbt -c "$DEVICE_NAME")" ]
+  then
+    echo "Doesn't look like device is connected: $DEVICE_NAME"
+    exit 5
+  fi
+else
+  # error if trying to connect and it's already connected
+  if [ "$(lsbt -c "$DEVICE_NAME")" ]
+  then
+    echo "Looks like device is already connected: $DEVICE_NAME"
+    exit 6
+  fi
 fi
 
 osascript  <<END
@@ -64,9 +105,41 @@ tell application "System Events"
 					end if
 				end tell
 			else
-				return "Device menu was not found, are headphones paired and not already connecting/disconnecting?"
+        return "Device menu was not found, are headphones paired and not already connecting/disconnecting?"
 			end if
 		end tell
 	end tell
 end tell
 END
+
+echo -n "Watching to see change in lsbt output..."
+stop_looking_for_change_at=$(expr $(date +%s) + $TIMEOUT_SECS)
+saw_change=""
+while [ ! "$saw_change" -a "$(date +%s)" -lt $stop_looking_for_change_at ]
+do
+  echo -n .
+  if [ "$action" = "Disconnect" ]
+  then
+    # We're done once it's not connected
+    if [ ! "$(lsbt -c "$DEVICE_NAME")" ]
+    then
+      echo
+      echo "Disconnected!"
+      saw_change=1
+    fi
+  else
+    # We're done once it's connected
+    if [ "$(lsbt -c "$DEVICE_NAME")" ]
+    then
+      echo
+      echo "Connected!"
+      saw_change=1
+    fi
+  fi
+done
+if [ ! "$saw_change" ]
+then
+  echo
+  echo "Timed out waiting for action"
+  exit 7
+fi
