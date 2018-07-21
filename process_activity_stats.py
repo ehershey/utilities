9bd092fee093650756e8adc43ffddd43052f75c9
Ernie Hershey <github@ernie.org>
2018-07-21 16:22:32 -0400

    improve debugging

--- process_activity_stats.py
+++ process_activity_stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 """
-
 Watch for tcx files saved by Tapiriik.
 When they come in, do some analysis and email results to myself.
 """
@@ -11,6 +10,7 @@ import os
 import erniemail
 import erniegps
 from pymongo import MongoClient
+
 
 ACTIVITY_DIR = "{0}/Dropbox/Apps/tapiriik".format(os.environ['HOME'])
 DB_URL = 'mongodb://localhost:27017/'
@@ -33,11 +33,15 @@ def main():
     parser = argparse.ArgumentParser(description='Process Tapiriik activities and send stats emails')
     parser.add_argument('--filter', required=False, help='Case insensitive regex to limit processed filenames', type=str)
     parser.add_argument('--redo', default=False, help='Process activity whether in DB already or not', action='store_true')
+    parser.add_argument('--debug', default=False, help='Print debugging info to stderr', action='store_true')
     args = parser.parse_args()
+
+    if args.debug:
+        logging.getLogger().setLevel(getattr(logging, "DEBUG"))
 
     client = MongoClient(DB_URL)
 
-    db = MongoClient(DB_URL)[DB_NAME]
+    db = client[DB_NAME]
 
     collection = db[COLLECTION_NAME]
 
@@ -54,6 +58,7 @@ def main():
             activity = collection.find_one(activity_query)
             create_activity = False
             create_activity_reason = None
+            logging.debug("activity: {0}".format(activity))
             if activity is None:
                 create_activity = True
                 create_activity_reason = "No activity in DB"
@@ -69,13 +74,14 @@ def main():
                 create_activity_reason = "Redo flag passed"
 
             if create_activity:
-                print("reading filename: {0}".format(filename))
+                logging.debug("Reading filename: {0}".format(filename))
                 activity = erniegps.read_activity(filename)
+                logging.debug("Processing activity: {0}".format(activity))
                 activity = erniegps.process_activity(activity)
 
                 activity['activity_version'] = ACTIVITY_VERSION
 
-                print("Created activity ( {0} )".format(create_activity_reason))
+                logging.debug("Created activity ( {0} )".format(create_activity_reason))
 
                 result = collection.replace_one(
                     activity_query, activity, upsert=True)
@@ -97,7 +103,7 @@ def main():
 
                 erniemail.activity_notify(activity, RECIPIENT, SENDER)
             else:
-                logging.debug("Found activity")
+                logging.debug("Found pre-existing activity in db")
 
         else:
             logging.debug("Skipping filename: {0}".format(filename))
