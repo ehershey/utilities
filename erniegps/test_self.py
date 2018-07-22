30ea80cd1e7888c05300c54d07b9fa53b7cd65f3
Ernie Hershey <github@ernie.org>
2018-07-22 12:44:53 -0400

    Test additions/fixups wip

--- erniegps/test_self.py
+++ erniegps/test_self.py
@@ -4,7 +4,10 @@
 Test erniegps module
 """
 
+import datetime
+import logging
 import os
+import pytz
 import erniegps
 import m26
 
@@ -134,3 +137,280 @@ def test_handle_unicode_notes_char():
     erniegps.read_activity(
         u'{0}/Dropbox/Apps/tapiriik/2016-06-01_Słomczyn Running_Running.tcx'.
         format(os.environ['HOME']))
+
+
+def test_fake_track():
+    """
+    Verify creating a simple track from scratch returns the expected results.
+
+    This is mainly useful for differentiating between problems with basic
+    tracks/analysis and more complicated track building/analysis in other tests
+    """
+
+    # Constant speed
+    trackpoints = []
+    for i in range(1, 100):
+        point = {"distancemiles": i * 10, "elapsedtime": "{0}:00:00".format(i)}
+        trackpoints.append(point)
+
+    speed = erniegps.get_speed_from_trackpoints(trackpoints)
+
+    assert speed.mph() == 10.0
+    assert speed.seconds_per_mile() == 360.0
+
+    is_negative_split = erniegps.get_is_negative_split(trackpoints)
+    assert not is_negative_split
+
+
+def test_end_negative_split():
+    """ Test negative split with one fast point at the end """
+
+    # how many points to use
+    #
+    point_count = 100
+
+    trackpoints = []
+    starttime = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    for i in range(0, point_count):
+        point = {
+            "distancemiles": i * 10,
+            "elapsedtime": "{0}:00:00".format(i),
+            "time": str(starttime + datetime.timedelta(hours=i))
+        }
+        trackpoints.append(point)
+
+    # Add one point with the same distance as between each previous point
+    # and only taking 30 minutes instead of an hour
+    #
+    endtime = starttime + datetime.timedelta(hours=point_count - 1, minutes=30)
+    trackpoints.append({
+        "distancemiles": point_count * 10,
+        "elapsedtime": "{0}:30:00".format(point_count - 1),
+        "time": str(endtime)
+    })
+
+    speed = erniegps.get_speed_from_trackpoints(trackpoints)
+
+    assert speed.mph() > 10.0
+    assert speed.seconds_per_mile() < 360.0
+
+    is_negative_split = erniegps.get_is_negative_split(trackpoints)
+    assert is_negative_split
+
+    activity = {
+        "trackpoints": trackpoints,
+        "activity_type": "running",
+        "notes": "Test activity created in code",
+        "starttime": str(starttime),
+        "endtime": str(endtime)
+        }
+    erniegps.decorate_activity(activity)
+    erniegps.process_activity(activity)
+
+    negative_split_depth = activity['negative_split_depth']
+    assert negative_split_depth >= 2
+
+
+def test_start_negative_split():
+    """ Simple negative split with one slow point at the start """
+
+    # how many points to use
+    #
+    point_count = 100
+
+    trackpoints = []
+    starttime = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    for i in range(0, point_count):
+        # constant distance per point but add one hour to the overall track
+        point = {
+            "distancemiles": i * 10,
+            "elapsedtime": "{0}:00:00".format(i + 1),
+            "time": str(starttime + datetime.timedelta(hours=i + 1))
+        }
+        trackpoints.append(point)
+
+    # Zero out first point so the second one is two hours later
+    #
+    endtime = trackpoints[-1]['time']
+
+    trackpoints[0]["elapsedtime"] = "00:00:00"
+    trackpoints[0]["time"] = str(starttime)
+
+    speed = erniegps.get_speed_from_trackpoints(trackpoints)
+
+    assert speed.mph() < 10.0
+    assert speed.seconds_per_mile() > 360.0
+
+    is_negative_split = erniegps.get_is_negative_split(trackpoints)
+    assert is_negative_split
+
+    activity = {
+        "trackpoints": trackpoints,
+        "activity_type": "running",
+        "notes": "Test activity created in code",
+        "starttime": str(starttime),
+        "endtime": str(endtime)
+        }
+    erniegps.decorate_activity(activity)
+    erniegps.process_activity(activity)
+
+    negative_split_depth = activity['negative_split_depth']
+    assert negative_split_depth == 1
+
+
+def test_advantage_split_slow_end(caplog):
+    """
+    Test detecting a negative split when slow points are at the back of a track
+
+    Constant track base + one fast point + one slow point
+
+    """
+    # how many points to use
+    #
+    point_count = 100
+
+    trackpoints = []
+    starttime = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    for i in range(0, point_count):
+        point = {
+            "distancemiles": i * 10,
+            "elapsedtime": "{0}:00:00".format(i),
+            "time": str(starttime + datetime.timedelta(hours=i))
+        }
+        trackpoints.append(point)
+
+    # Add one point with the same distance as between each previous point
+    # and only taking 45 minutes instead of an hour
+    #
+    trackpoints.append({
+        "distancemiles": point_count * 10,
+        "elapsedtime": "{0}:45:00".format(point_count - 1),
+        "time": str(starttime + datetime.timedelta(hours=point_count - 1, minutes=45))
+    })
+
+    simple_negative_split_speed = erniegps.get_speed_from_trackpoints(trackpoints)
+
+    simple_is_negative_split = erniegps.get_is_negative_split(trackpoints)
+    assert simple_is_negative_split
+
+    # Add one point with the same distance as between each previous point
+    # and taking two hours instead of one
+    #
+    endtime = starttime + datetime.timedelta(hours=point_count + 1, minutes=45)
+    trackpoints.append({
+        "distancemiles": (point_count + 1) * 10,
+        "elapsedtime": "{0}:45:00".format(point_count + 1),
+        "time": str(endtime)
+    })
+
+    advantage_negative_split_speed = erniegps.get_speed_from_trackpoints(trackpoints)
+
+    assert simple_negative_split_speed.mph() > advantage_negative_split_speed.mph()
+
+    assert advantage_negative_split_speed.mph() < 10.0
+    assert advantage_negative_split_speed.seconds_per_mile() > 360.0
+
+    caplog.set_level(logging.DEBUG)
+
+    advantage_is_negative_split = erniegps.get_is_negative_split(trackpoints)
+    assert advantage_is_negative_split
+
+    # For good measure, test failing to detect it without advantage allowed
+    is_simple_negative_split = erniegps.get_is_negative_split(
+        trackpoints, advantage_allowed=False)
+    assert not is_simple_negative_split
+
+    activity = {
+        "trackpoints": trackpoints,
+        "activity_type": "running",
+        "notes": "Test activity created in code",
+        "starttime": str(starttime),
+        "endtime": str(endtime)
+        }
+    erniegps.decorate_activity(activity)
+    erniegps.process_activity(activity)
+
+    negative_split_depth = activity['negative_split_depth']
+    assert negative_split_depth >= 2
+
+
+def test_adv_split_slow_start_end(caplog):
+    """
+    Test detecting a negative split when slow points are at the start of a track
+
+    Constant track base + one fast point + one slow point + slow start
+    """
+
+    # how many points to use
+    #
+    point_count = 100
+
+    trackpoints = []
+    starttime = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    for i in range(0, point_count):
+        # constant distance per point but add one hour to the overall track
+        point = {
+            "distancemiles": i * 10,
+            "elapsedtime": "{0}:00:00".format(i + 1),
+            "time": str(starttime + datetime.timedelta(hours=i + 1))
+        }
+        trackpoints.append(point)
+
+    # Zero out first point so the second one is two hours later
+    #
+    trackpoints[0]["elapsedtime"] = "00:00:00"
+    trackpoints[0]["time"] = str(starttime)
+
+    # Add one point with the same distance as between each previous point
+    # and only taking 45 minutes instead of an hour
+    #
+    trackpoints.append({
+        "distancemiles": point_count * 10,
+        "elapsedtime": "{0}:45:00".format(point_count + 1),
+        "time": str(starttime + datetime.timedelta(hours=point_count, minutes=45))
+    })
+
+    simple_negative_split_speed = erniegps.get_speed_from_trackpoints(trackpoints)
+
+    simple_is_negative_split = erniegps.get_is_negative_split(trackpoints)
+    assert simple_is_negative_split
+
+    # Add one point with the same distance as between each previous point
+    # and taking two hours instead of one
+    #
+    endtime = starttime + datetime.timedelta(hours=point_count + 2, minutes=45)
+    trackpoints.append({
+        "distancemiles": (point_count + 2) * 10,
+        "elapsedtime": "{0}:45:00".format(point_count + 2),
+        "time": str(endtime)
+    })
+
+    advantage_negative_split_speed = erniegps.get_speed_from_trackpoints(trackpoints)
+
+    assert simple_negative_split_speed.mph() > advantage_negative_split_speed.mph()
+
+    assert advantage_negative_split_speed.mph() < 10.0
+    assert advantage_negative_split_speed.seconds_per_mile() > 360.0
+
+    caplog.set_level(logging.DEBUG)
+
+    advantage_is_negative_split = erniegps.get_is_negative_split(trackpoints)
+    assert advantage_is_negative_split
+
+    # For good measure, test failing to detect it without advantage allowed
+    is_simple_negative_split = erniegps.get_is_negative_split(
+        trackpoints, advantage_allowed=False)
+    assert not is_simple_negative_split
+
+    activity = {
+        "trackpoints": trackpoints,
+        "activity_type": "running",
+        "notes": "Test activity created in code",
+        "starttime": str(starttime),
+        "endtime": str(endtime)
+        }
+    erniegps.decorate_activity(activity)
+    erniegps.process_activity(activity)
+
+    negative_split_depth = activity['negative_split_depth']
+    assert negative_split_depth >= 2
