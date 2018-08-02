2c20f62fe03f3ef907f275e493e60c17a754a956
Ernie Hershey <github@ernie.org>
2018-08-02 16:25:40 -0400

    R.I.P. Moves

--- update_moves_summaries.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Call the python file, passing spreadsheet info and a limit of 1
-#
-# This is intended to be called in limited circumstances when updating one value is expected
-#
-#
-. "$(dirname "$0")"/crontab.env
-"$(dirname "$0")"/update_moves_summaries.py --spreadsheet_id $MARATHON_SPREADSHEET_ID --sheet_name "All moves data" --limit 1
