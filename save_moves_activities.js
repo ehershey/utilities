ea8e08ea7d4c75abb6473c33748729a02ab5a9e1
Ernie Hershey <github@ernie.org>
2018-08-02 16:23:17 -0400

    fixes, cleanup,  save_moves_activities.js, update to moves api v1.1

--- save_moves_activities.js
+++ save_moves_activities.js
@@ -19,7 +19,7 @@
 //
 //
 // Example usage in crontab:
-// */5 * * * * curl --silent "https://api.moves-app.com/api/v1/user/activities/daily?pastDays=7&access_token=$MOVES_ACCESS_TOKEN"  | ~ernie/git/utilities/save_moves_activities.js > /tmp/save_moves_activities.log 2>&1
+// */5 * * * * curl --silent "https://api.moves-app.com/api/1.1/user/activities/daily?pastDays=7&access_token=$MOVES_ACCESS_TOKEN"  | ~ernie/git/utilities/save_moves_activities.js > /tmp/save_moves_activities.log 2>&1
 
 
 var mongodb = require('mongodb');
@@ -70,7 +70,7 @@ var MongoClient = mongodb.MongoClient;
 // read json on stdin into memory
 //
 
-var stdin_chunks = []
+var stdin_chunks = [];
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
@@ -84,81 +84,37 @@ var now = new Date();
 
 var last_seen = now;
 
-var wrote_entry_count = 0;
-var total_entry_count = 0;
+var wrote_date_count = 0;
+var total_date_count = 0;
 var wrote_segment_count = 0;
 var total_segment_count = 0;
 var wrote_activity_count = 0;
 var total_activity_count = 0;
 var incoming_json;
 
-process.stdin.on('end', function() {
-  var incoming_string = stdin_chunks.join("");
-  incoming_json = JSON.parse(incoming_string);
+
+function cleanup_db_connection(db) {
+  setTimeout(function() {
+    if(wrote_activity_count === total_activity_count && wrote_date_count === total_date_count && wrote_segment_count === total_segment_count)
+    {
+      console.log('calling db.close()');
+      db.close();
+    }
+  }, 5000);
+}
 
 
-  // connect to db and store data
-  //
-
-  MongoClient.connect(dburl, function(err, db)
-  {
-    if (err) throw err;
-
-    console.log("incoming_json.length: " + incoming_json.length);
-
-    for(var i = 0 ; i < incoming_json.length; i++) {
-
-      var entry = incoming_json[i];
-
-      save_day_entry(db,entry);
-      total_entry_count++;
-
-
-      request('https://api.moves-app.com/api/v1/user/storyline/daily/' + entry.date + '?trackPoints=true&access_token=' + moves_access_token,
-        function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var storyline = JSON.parse(body)[0];
-
-            var segments = storyline.segments;
-            console.log('segments.length: ' + segments.length);
-
-            if(!segments) { segments = [] }
-
-              for(var j = 0 ; j < segments.length ; j++)
-              {
-                total_segment_count++;
-                var segment = segments[j];
-                save_segment(db, segment);
-
-                var activities = segment.activities;
-
-                if(!activities) { activities = [] }
-
-                for(var k = 0 ; k < activities.length ; k++)
-                {
-                  total_activity_count++;
-                  var activity = activities[k];
-                  save_activity(db, activity);
-                }
-               }
-          }
-        });
-
-      }
-  });
-});
-
-function save_day_entry(db,entry)
+function save_date(db,date)
 {
 
   var collection = db.collection("dates");
 
-  collection.find({ date: entry.date}, function(err, cursor)
+  collection.find({ date: date.date}, function(err, cursor)
   {
-    if (err) throw err;
+    if (err) { throw err; }
 
 
-    console.log("entry.date: " + entry.date);
+    console.log("date.date: " + date.date);
 
     console.log('calling find() on dates');
 
@@ -168,7 +124,7 @@ function save_day_entry(db,entry)
 
     cursor.toArray(function(err, result)
     {
-      if (err) throw err;
+      if (err) { throw err; }
 
 
       console.log('result.length: ' + result.length);
@@ -187,29 +143,30 @@ function save_day_entry(db,entry)
       }
       else
       {
-        throw("too many results!");
         console.log("result[0].application_metadata: ");
         console.log(result[0].application_metadata);
         first_seen = result[0].application_metadata.first_seen;
         seen_count = result[0].application_metadata.seen_count + 1;
+        throw("too many results!");
       }
 
       console.log('first_seen: ' + first_seen);
       console.log('last_seen: ' + last_seen);
       console.log('seen_count: ' + seen_count);
 
-      entry.application_metadata = { first_seen: first_seen, last_seen: last_seen, seen_count: seen_count };
+      date.application_metadata = { first_seen: first_seen, last_seen: last_seen, seen_count: seen_count };
 
 
-      console.log('attempting to save entry: ' + entry);
+      console.log('attempting to save date: ' + date);
 
-      collection.update({date: entry.date}, entry, {upsert:true, w: 1}, function(err, result)
+      collection.update({date: date.date}, date, {upsert:true, w: 1}, function(err, result)
       {
-        if (err) throw err;
-        // collection.insert(entry, {upsert:true, w: 1}, function(err, result) {
+        if (err) { throw err; }
+        // collection.insert(date, {upsert:true, w: 1}, function(err, result) {
 
-        console.log("saved entry");
-        wrote_entry_count++;
+        console.log("saved date, result.result:");
+        console.log(result.result);
+        wrote_date_count++;
         cleanup_db_connection(db);
 
       });
@@ -224,7 +181,7 @@ function save_segment(db,segment)
 
   collection.find({ startTime: segment.startTime}, function(err, cursor)
   {
-    if (err) throw err;
+    if (err) { throw err; }
 
     console.log("segment.startTime: " + segment.startTime);
 
@@ -235,7 +192,7 @@ function save_segment(db,segment)
 
     cursor.toArray(function(err, result)
     {
-      if (err) throw err;
+      if (err) { throw err; }
 
       console.log('result.length: ' + result.length);
 
@@ -253,11 +210,11 @@ function save_segment(db,segment)
       }
       else
       {
-        throw("too many results!");
         console.log("result[0].application_metadata: ");
         console.log(result[0].application_metadata);
         first_seen = result[0].application_metadata.first_seen;
         seen_count = result[0].application_metadata.seen_count + 1;
+        throw("too many results!");
       }
 
       console.log('first_seen: ' + first_seen);
@@ -270,10 +227,11 @@ function save_segment(db,segment)
 
       collection.update({startTime: segment.startTime}, segment, {upsert:true, w: 1}, function(err, result)
       {
-        if (err) throw err;
+        if (err) { throw err; }
         // collection.insert(segment, {upsert:true, w: 1}, function(err, result) {
 
-        console.log("saved segment");
+        console.log("saved segment, result.result:");
+        console.log(result.result);
         wrote_segment_count++;
         cleanup_db_connection(db);
       });
@@ -281,14 +239,13 @@ function save_segment(db,segment)
   });
 }
 
-function save_activity(db,activity)
-{
+function save_activity(db,activity) {
 
   var collection = db.collection("activities");
 
   collection.find({ startTime: activity.startTime}, function(err, cursor)
   {
-    if (err) throw err;
+    if (err) { throw err; }
 
     console.log("activity.startTime: " + activity.startTime);
 
@@ -299,7 +256,7 @@ function save_activity(db,activity)
 
     cursor.toArray(function(err, result)
     {
-      if (err) throw err;
+      if (err) { throw err; }
 
       console.log('result.length: ' + result.length);
 
@@ -317,11 +274,11 @@ function save_activity(db,activity)
       }
       else
       {
-        throw("too many results!");
         console.log("result[0].application_metadata: ");
         console.log(result[0].application_metadata);
         first_seen = result[0].application_metadata.first_seen;
         seen_count = result[0].application_metadata.seen_count + 1;
+        throw("too many results!");
       }
 
       console.log('first_seen: ' + first_seen);
@@ -334,10 +291,10 @@ function save_activity(db,activity)
 
       collection.update({startTime: activity.startTime}, activity, {upsert:true, w: 1}, function(err, result)
       {
-        if (err) throw err;
+        if (err) { throw err; }
         // collection.insert(activity, {upsert:true, w: 1}, function(err, result) {
-
-        console.log("saved activity");
+        console.log("saved activity, result.result:");
+        console.log(result.result);
         wrote_activity_count++;
         cleanup_db_connection(db);
       });
@@ -345,12 +302,88 @@ function save_activity(db,activity)
   });
 }
 
-function cleanup_db_connection(db) {
-  setTimeout(function() {
-    if(wrote_activity_count === total_activity_count && wrote_entry_count === total_entry_count && wrote_segment_count === total_segment_count)
-    {
-      console.log('calling db.close()');
-      db.close();
+
+
+process.stdin.on('end', function() {
+  var incoming_string = stdin_chunks.join("");
+  var url;
+  incoming_json = JSON.parse(incoming_string);
+
+  if(incoming_json.length === 0 || incoming_json.length === undefined) {
+    throw "Invalid json: " + incoming_string;
+  }
+
+
+  // connect to db and store data
+  //
+
+  MongoClient.connect(dburl, function(err, db)
+  {
+    var i, date;
+    if (err) { throw err; }
+    function process_api_response (error, response, body) {
+      if(error) { throw error; }
+      if(response.statusCode !== 200) {
+        throw "Response status not 200 (" + response.statusCode + "), body: " + body;
+      }
+      var j;
+      var segment;
+      var segments;
+      var activity;
+      var activities;
+      var storyline;
+      var k;
+      if (!error && response.statusCode === 200) {
+        storyline = JSON.parse(body)[0];
+
+        segments = storyline.segments;
+        console.log('segments.length: ' + segments.length);
+
+        if(!segments) { segments = []; }
+
+          for(j = 0 ; j < segments.length ; j++)
+          {
+            total_segment_count++;
+            segment = segments[j];
+            save_segment(db, segment);
+
+            activities = segment.activities;
+
+            if(!activities) { activities = []; }
+
+            for(k = 0 ; k < activities.length ; k++)
+            {
+              total_activity_count++;
+              activity = activities[k];
+              save_activity(db, activity);
+            }
+           }
+      }
     }
-  }, 5000);
-}
+
+
+    console.log("incoming_json.length: " + incoming_json.length);
+
+    // one item per day
+    //
+
+    for(i = 0 ; i < incoming_json.length; i++) {
+
+      date = incoming_json[i];
+
+      save_date(db,date);
+      total_date_count++;
+
+
+      url = 'https://api.moves-app.com/api/1.1/user/storyline/daily/' + date.date + '?trackPoints=true&access_token=' + moves_access_token;
+      console.log("requesting " + url);
+      request(url, process_api_response);
+
+
+      }
+  });
+});
+
+
+
+
