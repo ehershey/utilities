3510995a14de00f16eb6782eef857800b2cb4f46
Ernie Hershey <github@ernie.org>
2019-03-27 14:57:22 -0400

    add metro and tram

--- erniegps/calories.py
+++ erniegps/calories.py
@@ -4,11 +4,13 @@ CALORIES_PER_MILE_BY_ACTIVITY_TYPE = {
     'cycling': 40,
     'bus': 0,
     'train': 0,
+    'metro': 0,
     'transport': 0,
     'car': 0,
     'stationary': 0,
     'airplane': 0,
     'motorcycle': 0,
     'none': 0,
-    'boat': 0
+    'boat': 0,
+    'tram': 0
     }
