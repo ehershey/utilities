2cdab29bacfc2c342804ea329f23389dbf6ecc1e
Ernie Hershey <github@ernie.org>
2020-10-06 11:47:52 -0400

    Diff and send mail when updating

--- generate_unit_report.sh
+++ generate_unit_report.sh
@@ -20,10 +20,12 @@ else
   md5=cat
 fi
 
+runtimestamp=$(date +%Y%m%d%H%M%S)
+
 tempfile=$(mktemp /tmp/average_unit_report.XXXXXX)
 trap "rm $tempfile" exit
 
-$SCRIPT > "$tempfile"
+$SCRIPT --run-timestamp "$runtimestamp" > "$tempfile"
 
 old_md5=$(grep -v diffignore "$REPORT" | $md5)
 new_md5=$(grep -v diffignore "$tempfile" | $md5)
@@ -34,8 +36,16 @@ echo "new_md5: $new_md5"
 if [ -s "$tempfile" -a "$old_md5" != "$new_md5" ]
 then
   echo "Generating new report"
+  echo "DIFF:"
+  diff "$REPORT" "$tempfile" || true
+  ls -l "$tempfile"
+  echo "old_md5: $old_md5"
+  echo "new_med5: $new_md5"
+  cp -pri "$REPORT" "$REPORT.$runtimestamp"
   cat "$tempfile" > "$REPORT"
+  date="$(date +%Y-%m-%d)"
+  (echo "Updated https://goeverywhere.ernie.org/unit_report.html" ; echo ; echo ; diff "$REPORT.$runtimestamp" "$REPORT" ) | mail -s "Unit Report Update $date" unit-notifier@ernie.org
 else
   echo "Not generating new report"
 fi
-
+find `dirname $REPORT` -path $REPORT.\* -mtime +7 -exec rm {} \;
