18a82cd04004957fbf02ab70f6a063e7fcb1e62e
Ernie Hershey <github@ernie.org>
2020-11-16 18:26:12 -0500

    add swimming calorie calculation

--- erniegps/calories.py
+++ erniegps/calories.py
@@ -1,6 +1,11 @@
 CALORIES_PER_MILE_BY_ACTIVITY_TYPE = {
     'bogus': 0,
     'running': 110,
+    'swimming': 440,
+    # http://www.marathonguide.com/training/coachmindy/crosstraining.cfm -
+    # The run to swim ratio is about 4:1, meaning that four miles of running equals one mile of
+    # swimming. At 8:00 to 10:00 pace, you'd be running 32 to 40 minutes (respectively)
+    # for approximately a four-mile run to equate to one mile of swimming.
     'walking': 100,
     'hiking': 105,
     'cycling': 40,
